f97d308a6b3448a8abb7439239595a3c268da307
santong <weipeng.swp@alibaba-inc.com>
2020-12-08 23:24:00 +0800

    optimize protoc.sh

--- build/protoc.sh
+++ build/protoc.sh
@@ -4,12 +4,15 @@ SRC="$(cd "$(dirname "$0")/.." && pwd)"
 
 echo "work dir: $SRC"
 
-protoc -I="$SRC" --go_out=plugins=grpc,paths=source_relative:"$SRC" \
+if protoc -I="$SRC" \
+  --go_out "$SRC" --go_opt paths=source_relative \
+  --go-grpc_out "$SRC" --go-grpc_opt paths=source_relative \
   "$SRC"/pkg/grpc/base/*.proto \
   "$SRC"/pkg/grpc/cdnsystem/*.proto \
   "$SRC"/pkg/grpc/dfdaemon/*.proto \
-  "$SRC"/pkg/grpc/scheduler/*.proto
-
+  "$SRC"/pkg/grpc/scheduler/*.proto; then
+  echo "generate grpc code successfully"
+else
+  echo "generate grpc code fail"
+fi
 cd "$SRC" && go mod tidy
-
-echo "generate grpc code successfully"
