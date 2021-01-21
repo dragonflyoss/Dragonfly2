4b4aee6b232940f6ac21fb9ae92f52f500b9cf79
Jim Ma <majinjing3@gmail.com>
2021-01-21 20:25:17 +0800

    rpc: add custom listner support
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- pkg/rpc/server.go
+++ pkg/rpc/server.go
@@ -20,14 +20,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/dragonflyoss/Dragonfly2/pkg/basic/dfnet"
-	logger "github.com/dragonflyoss/Dragonfly2/pkg/dflog"
-	"github.com/dragonflyoss/Dragonfly2/pkg/util/fileutils"
-	"github.com/dragonflyoss/Dragonfly2/pkg/util/stringutils"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/keepalive"
-	"google.golang.org/grpc/status"
 	"net"
 	"os"
 	"strconv"
@@ -35,6 +27,16 @@ import (
 	"sync"
 	"syscall"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/status"
+
+	"github.com/dragonflyoss/Dragonfly2/pkg/basic/dfnet"
+	logger "github.com/dragonflyoss/Dragonfly2/pkg/dflog"
+	"github.com/dragonflyoss/Dragonfly2/pkg/util/fileutils"
+	"github.com/dragonflyoss/Dragonfly2/pkg/util/stringutils"
 )
 
 type RegisterFunc func(*grpc.Server, interface{})
@@ -230,4 +232,54 @@ func unaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.Una
 	}
 
 	return m, err
+}
+
+// Listen wraps net.Listen with dfnet.NetAddr
+// Example:
+//    Listen(dfnet.NetAddr{Type: dfnet.UNIX, Addr: "/var/run/df.sock"})
+//    Listen(dfnet.NetAddr{Type: dfnet.TCP, Addr: ":12345"})
+func Listen(netAddr dfnet.NetAddr) (net.Listener, error) {
+	return net.Listen(string(netAddr.Type), netAddr.Addr)
+}
+
+// ListenWithPortRange tries to listen a port between startPort and endPort, return net.Listener and listen port
+// Example:
+//    ListenWithPortRange("0.0.0.0", 12345, 23456)
+//    ListenWithPortRange("192.168.0.1", 12345, 23456)
+//    ListenWithPortRange("192.168.0.1", 0, 0) // random port
+func ListenWithPortRange(listen string, startPort, endPort int64) (net.Listener, int, error) {
+	for ; startPort <= endPort; startPort++ {
+		listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", listen, startPort))
+		if err == nil && listener != nil {
+			addr := listener.Addr().String()
+			index := strings.LastIndex(addr, ":")
+			p, cerr := strconv.Atoi(stringutils.SubString(addr, index+1, len(addr)))
+			if cerr != nil {
+				continue
+			}
+			return listener, p, nil
+		}
+		if isErrAddrInuse(err) {
+			continue
+		} else if err != nil {
+			return nil, -1, err
+		}
+	}
+	return nil, -1, fmt.Errorf("")
+}
+
+type Server interface {
+	Serve(net.Listener) error
+	Stop()
+	GracefulStop()
+}
+
+// NewServer returns a Server with impl
+// Example:
+//    s := NewServer(impl, ...)
+//    s.Serve(listener)
+func NewServer(impl interface{}, opts ...grpc.ServerOption) Server {
+	server := grpc.NewServer(append(serverOpts, opts...)...)
+	register(server, impl)
+	return server
 }
