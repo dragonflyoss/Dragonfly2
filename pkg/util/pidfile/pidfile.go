a286bd27ef18a4f843c678b2b4eebe9bf3b31523
Jim Ma <majinjing3@gmail.com>
2021-01-25 13:20:29 +0800

    daemon: add pkg/util/pidfile/
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

---
+++ pkg/util/pidfile/pidfile.go
@@ -0,0 +1,56 @@
+// Package pidfile provides structure and helper functions to create and remove
+// PID file. A PID file is usually a file used to store the process ID of a
+// running process.
+package pidfile // copy from "github.com/docker/docker/pkg/pidfile"
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/shirou/gopsutil/v3/process"
+)
+
+// PIDFile is a file used to store the process ID of a running process.
+type PIDFile struct {
+	path string
+}
+
+func IsProcessExistsByPIDFile(path string) (bool, error) {
+	pidByte, err := ioutil.ReadFile(path)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	pid, err := strconv.Atoi(strings.TrimSpace(string(pidByte)))
+	if err != nil {
+		return false, err
+	}
+	return process.PidExists(int32(pid))
+}
+
+// New creates a PIDfile using the specified path.
+func New(path string) (*PIDFile, error) {
+	if ok, err := IsProcessExistsByPIDFile(path); ok || err != nil {
+		return nil, fmt.Errorf("process already exists or with error %s", err)
+	}
+	// Note MkdirAll returns nil if a directory already exists
+	if err := os.MkdirAll(filepath.Dir(path), os.FileMode(0755)); err != nil {
+		return nil, err
+	}
+	if err := ioutil.WriteFile(path, []byte(fmt.Sprintf("%d", os.Getpid())), 0644); err != nil {
+		return nil, err
+	}
+
+	return &PIDFile{path: path}, nil
+}
+
+// Remove removes the PIDFile.
+func (file PIDFile) Remove() error {
+	return os.Remove(file.path)
+}
