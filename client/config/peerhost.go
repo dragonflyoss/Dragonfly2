5f14209f72eedb680ab047695385553811ef8801
gaius-qi <gaius.qi@gmail.com>
2021-02-05 12:33:12 +0800

    fix: CACert
    Signed-off-by: gaius-qi <gaius.qi@gmail.com>

--- client/config/peerhost.go
+++ client/config/peerhost.go
@@ -312,10 +312,10 @@ type UnixListenOption struct {
 }
 
 type SecurityOption struct {
-	Insecure  bool        `json:"insecure"`
-	CACert    string      `json:"ca_cert"`
-	Cert      string      `json:"cert"`
-	Key       string      `json:"key"`
+	Insecure  bool        `json:"insecure" yaml:"insecure"`
+	CACert    string      `json:"ca_cert" yaml:"ca_cert"`
+	Cert      string      `json:"cert" yaml:"cert"`
+	Key       string      `json:"key" yaml:"key"`
 	TLSConfig *tls.Config `json:"tls_config" yaml:"tls_config"`
 }
 
