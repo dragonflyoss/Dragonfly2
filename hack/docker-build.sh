48eb7ce3ee124dbe0ce5bbda0452f6d432e7fc8a
Jim Ma <majinjing3@gmail.com>
2021-02-07 15:14:42 +0800

    update build script
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -4,12 +4,12 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-DF_VERSION=${DF_VERSION:-"latest"}
+D7Y_VERSION=${D7Y_VERSION:-"latest"}
 curDir=$(cd "$(dirname "$0")" && pwd)
 cd "${curDir}/../" || return
 
 docker-build::build-cdn(){
-    docker build --build-arg GOPROXY="${GOPROXY}" -t cdn:"${DF_VERSION}" -f Dockerfile.cdn .
+    docker build --build-arg GOPROXY="${GOPROXY}" -t cdn:"${D7Y_VERSION}" -f Dockerfile.cdn .
 }
 
 main() {
