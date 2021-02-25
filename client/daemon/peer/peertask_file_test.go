d134d0916650d6c5b31ae4ba3e520143471ea1f8
Jim Ma <majinjing3@gmail.com>
2021-02-25 22:05:07 +0800

    daemon: update back source test
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

---
+++ client/daemon/peer/peertask_file_test.go
@@ -0,0 +1,211 @@
+package peer
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	testifyassert "github.com/stretchr/testify/assert"
+
+	"d7y.io/dragonfly/v2/cdnsystem/types"
+	"d7y.io/dragonfly/v2/client/daemon/gc"
+	"d7y.io/dragonfly/v2/client/daemon/test"
+	"d7y.io/dragonfly/v2/client/daemon/test/mock/source"
+	"d7y.io/dragonfly/v2/pkg/rpc/scheduler"
+)
+
+func TestFilePeerTask_BackSource_WithContentLength(t *testing.T) {
+	assert := testifyassert.New(t)
+	ctrl := gomock.NewController(t)
+
+	testBytes, err := ioutil.ReadFile(test.File)
+	assert.Nil(err, "load test file")
+
+	var (
+		pieceParallelCount = int32(4)
+		pieceSize          = 1024
+
+		mockContentLength = len(testBytes)
+		//mockPieceCount    = int(math.Ceil(float64(mockContentLength) / float64(pieceSize)))
+
+		peerID = "peer-0"
+		taskID = "task-0"
+
+		output = "../test/testdata/test.output"
+		url    = "http://localhost/test/data"
+	)
+	defer os.Remove(output)
+
+	schedulerClient, storageManager := setupPeerTaskManagerComponents(ctrl, taskID, int64(mockContentLength), int32(pieceSize), pieceParallelCount)
+	defer storageManager.(gc.GC).TryGC()
+
+	downloader := NewMockPieceDownloader(ctrl)
+	downloader.EXPECT().DownloadPiece(gomock.Any()).AnyTimes().DoAndReturn(func(task *DownloadPieceRequest) (io.Reader, io.Closer, error) {
+		rc := ioutil.NopCloser(
+			bytes.NewBuffer(
+				testBytes[task.piece.RangeStart : task.piece.RangeStart+uint64(task.piece.RangeSize)],
+			))
+		return rc, rc, nil
+	})
+
+	sourceClient := source.NewMockResourceClient(ctrl)
+	sourceClient.EXPECT().GetContentLength(url, map[string]string{}).DoAndReturn(
+		func(url string, headers map[string]string) (int64, error) {
+			return int64(len(testBytes)), nil
+		})
+	sourceClient.EXPECT().Download(url, map[string]string{}).DoAndReturn(
+		func(url string, headers map[string]string) (*types.DownloadResponse, error) {
+			return &types.DownloadResponse{
+				Body: ioutil.NopCloser(bytes.NewBuffer(testBytes)),
+			}, nil
+		})
+
+	ptm := &peerTaskManager{
+		host: &scheduler.PeerHost{
+			Ip: "127.0.0.1",
+		},
+		runningPeerTasks: sync.Map{},
+		pieceManager: &pieceManager{
+			storageManager:  storageManager,
+			pieceDownloader: downloader,
+		},
+		storageManager: storageManager,
+		scheduler:      schedulerClient,
+	}
+	req := &FilePeerTaskRequest{
+		PeerTaskRequest: scheduler.PeerTaskRequest{
+			Url:      "http://localhost/test/data",
+			Filter:   "",
+			BizId:    "d7y-test",
+			UrlMata:  nil,
+			PeerId:   peerID,
+			PeerHost: &scheduler.PeerHost{},
+		},
+		Output: output,
+	}
+	ctx := context.Background()
+	pt, err := NewFilePeerTask(ctx, ptm.host, ptm.scheduler, ptm.pieceManager, &req.PeerTaskRequest)
+	assert.Nil(err, "new file peer task")
+
+	pt.SetCallback(&filePeerTaskCallback{
+		ctx:   ctx,
+		ptm:   ptm,
+		req:   req,
+		start: time.Now(),
+	})
+
+	progress, err := pt.Start(ctx)
+	assert.Nil(err, "start file peer task")
+
+	var p *PeerTaskProgress
+	for p = range progress {
+		assert.True(p.State.Success)
+	}
+	assert.NotNil(p)
+	assert.True(p.Done)
+
+	outputBytes, err := ioutil.ReadFile(output)
+	assert.Nil(err, "load output file")
+	assert.Equal(testBytes, outputBytes, "output and desired output must match")
+}
+
+func TestFilePeerTask_BackSource_WithoutContentLength(t *testing.T) {
+	assert := testifyassert.New(t)
+	ctrl := gomock.NewController(t)
+
+	testBytes, err := ioutil.ReadFile(test.File)
+	assert.Nil(err, "load test file")
+
+	var (
+		pieceParallelCount = int32(4)
+		pieceSize          = 1024
+
+		mockContentLength = len(testBytes)
+		//mockPieceCount    = int(math.Ceil(float64(mockContentLength) / float64(pieceSize)))
+
+		peerID = "peer-0"
+		taskID = "task-0"
+
+		output = "../test/testdata/test.output"
+		url    = "http://localhost/test/data"
+	)
+	defer os.Remove(output)
+
+	schedulerClient, storageManager := setupPeerTaskManagerComponents(ctrl, taskID, int64(mockContentLength), int32(pieceSize), pieceParallelCount)
+	defer storageManager.(gc.GC).TryGC()
+
+	downloader := NewMockPieceDownloader(ctrl)
+	downloader.EXPECT().DownloadPiece(gomock.Any()).AnyTimes().DoAndReturn(func(task *DownloadPieceRequest) (io.Reader, io.Closer, error) {
+		rc := ioutil.NopCloser(
+			bytes.NewBuffer(
+				testBytes[task.piece.RangeStart : task.piece.RangeStart+uint64(task.piece.RangeSize)],
+			))
+		return rc, rc, nil
+	})
+
+	sourceClient := source.NewMockResourceClient(ctrl)
+	sourceClient.EXPECT().GetContentLength(url, map[string]string{}).DoAndReturn(
+		func(url string, headers map[string]string) (int64, error) {
+			return -1, nil
+		})
+	sourceClient.EXPECT().Download(url, map[string]string{}).DoAndReturn(
+		func(url string, headers map[string]string) (*types.DownloadResponse, error) {
+			return &types.DownloadResponse{
+				Body: ioutil.NopCloser(bytes.NewBuffer(testBytes)),
+			}, nil
+		})
+
+	ptm := &peerTaskManager{
+		host: &scheduler.PeerHost{
+			Ip: "127.0.0.1",
+		},
+		runningPeerTasks: sync.Map{},
+		pieceManager: &pieceManager{
+			storageManager:  storageManager,
+			pieceDownloader: downloader,
+		},
+		storageManager: storageManager,
+		scheduler:      schedulerClient,
+	}
+	req := &FilePeerTaskRequest{
+		PeerTaskRequest: scheduler.PeerTaskRequest{
+			Url:      "http://localhost/test/data",
+			Filter:   "",
+			BizId:    "d7y-test",
+			UrlMata:  nil,
+			PeerId:   peerID,
+			PeerHost: &scheduler.PeerHost{},
+		},
+		Output: output,
+	}
+	ctx := context.Background()
+	pt, err := NewFilePeerTask(ctx, ptm.host, ptm.scheduler, ptm.pieceManager, &req.PeerTaskRequest)
+	assert.Nil(err, "new file peer task")
+
+	pt.SetCallback(&filePeerTaskCallback{
+		ctx:   ctx,
+		ptm:   ptm,
+		req:   req,
+		start: time.Now(),
+	})
+
+	progress, err := pt.Start(ctx)
+	assert.Nil(err, "start file peer task")
+
+	var p *PeerTaskProgress
+	for p = range progress {
+		assert.True(p.State.Success)
+	}
+	assert.NotNil(p)
+	assert.True(p.Done)
+
+	outputBytes, err := ioutil.ReadFile(output)
+	assert.Nil(err, "load output file")
+	assert.Equal(testBytes, outputBytes, "output and desired output must match")
+}
