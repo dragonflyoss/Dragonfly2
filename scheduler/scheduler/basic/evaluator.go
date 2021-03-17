264632d255d238327d88a3921916f9faccb4371a
李玉海 <yuhai.lyh@alibaba-inc.com>
2021-03-17 11:12:38 +0800

    scheduler add log

--- scheduler/scheduler/basic/evaluator.go
+++ scheduler/scheduler/basic/evaluator.go
@@ -20,6 +20,8 @@ import (
 	logger "d7y.io/dragonfly/v2/pkg/dflog"
 	"d7y.io/dragonfly/v2/scheduler/mgr"
 	"d7y.io/dragonfly/v2/scheduler/types"
+	"fmt"
+	"strings"
 	"time"
 )
 
@@ -132,18 +134,26 @@ func (e *Evaluator) SelectChildCandidates(peer *types.PeerTask) (list []*types.P
 
 func (e *Evaluator) SelectParentCandidates(peer *types.PeerTask) (list []*types.PeerTask) {
 	if peer == nil {
+		logger.Debugf("peerTask is nil")
 		return
 	}
+	var msg []string
 	mgr.GetPeerTaskManager().Walker(func(pt *types.PeerTask) bool {
-		if pt == nil || peer.Task != pt.Task {
+		if pt == nil {
+			return true
+		} else if peer.Task != pt.Task {
+			msg = append(msg, fmt.Sprintf("%s task[%s] not same", pt.Pid, pt.Task.TaskId))
 			return true
 		} else if pt.IsDown() {
+			msg = append(msg, fmt.Sprintf("%s is down", pt.Pid))
 			return true
 		} else if pt.Pid == peer.Pid {
 			return true
 		} else if pt.IsAncestor(peer) || peer.IsAncestor(pt) {
+			msg = append(msg, fmt.Sprintf("%s has relation", pt.Pid))
 			return true
 		} else if pt.GetFreeLoad() < 1 {
+			msg = append(msg, fmt.Sprintf("%s no load", pt.Pid))
 			return true
 		}
 		if pt.Success {
@@ -152,10 +162,16 @@ func (e *Evaluator) SelectParentCandidates(peer *types.PeerTask) (list []*types.
 			root := pt.GetRoot()
 			if root != nil && root.Host != nil && root.Host.Type == types.HostTypeCdn {
 				list = append(list, pt)
+			} else {
+				msg = append(msg, fmt.Sprintf("%s not finished and root is not cdn", pt.Pid))
 			}
 		}
 		return true
 	})
+	if len(list) == 0 {
+		logger.Debugf("[%s][%s] %s", peer.Task.TaskId, peer.Pid, strings.Join(msg, "\n"))
+	}
+
 	return
 }
 
