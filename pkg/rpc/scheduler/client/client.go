5504a397194423ebc0c2c539aad8416f61a62fb2
santong <weipeng.swp@alibaba-inc.com>
2021-03-18 14:39:06 +0800

    grpc client manage
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/rpc/scheduler/client/client.go
+++ pkg/rpc/scheduler/client/client.go
@@ -41,16 +41,12 @@ var sc *schedulerClient
 
 var once sync.Once
 
-func init()  {
+func GetClientByAddr(addrs []dfnet.NetAddr) (SchedulerClient, error) {
 	once.Do(func() {
 		sc = &schedulerClient{
 			rpc.NewConnection(make([]dfnet.NetAddr, 0)),
 		}
 	})
-}
-
-
-func GetClientByAddr(addrs []dfnet.NetAddr) (SchedulerClient, error) {
 	if len(addrs) == 0 {
 		return nil, errors.New("address list of cdn is empty")
 	}
