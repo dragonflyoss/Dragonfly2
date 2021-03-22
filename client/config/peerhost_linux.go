c00b350e221e4bca7f001d892dbfe0d4d9ae3250
Gaius <gaius.qi@gmail.com>
2021-03-22 17:28:01 +0800

    test: config

--- client/config/peerhost_linux.go
+++ client/config/peerhost_linux.go
@@ -1,19 +1,3 @@
-/*
- * Copyright The Dragonfly Authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // +build linux
 
 package config
@@ -30,7 +14,7 @@ import (
 )
 
 var (
-	PeerHostConfigPath = "/etc/dragonfly/dfget-daemon.yaml"
+	PeerHostConfigPath = "/etc/dragonfly/dfget-daemon.yml"
 
 	peerHostWorkHome = "/etc/dragonfly/dfdaemon/"
 	peerHostDataDir  = peerHostWorkHome
