1a5181990f29ceeed33a0a6baed2179ffd676adb
santong <weipeng.swp@alibaba-inc.com>
2021-03-30 17:22:31 +0800

    refactor proto
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- build/protoc.sh
+++ build/protoc.sh
@@ -7,10 +7,10 @@ echo "work dir:$SRC"
 if protoc -I="$SRC" \
   --go_out "$SRC" --go_opt paths=source_relative \
   --go-grpc_out "$SRC" --go-grpc_opt paths=source_relative \
-#  "$SRC"/pkg/rpc/base/*.proto \
-#  "$SRC"/pkg/rpc/cdnsystem/*.proto \
-#  "$SRC"/pkg/rpc/dfdaemon/*.proto \
-#  "$SRC"/pkg/rpc/scheduler/*.proto \
+  "$SRC"/pkg/rpc/base/*.proto \
+  "$SRC"/pkg/rpc/cdnsystem/*.proto \
+  "$SRC"/pkg/rpc/dfdaemon/*.proto \
+  "$SRC"/pkg/rpc/scheduler/*.proto \
   "$SRC"/pkg/rpc/manager/*.proto; then
   echo "generate grpc code success"
   if cd "$SRC" && go mod tidy; then
