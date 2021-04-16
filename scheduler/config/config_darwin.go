8133ed32961b18d0489820b457a695714efc8424
Gaius <gaius.qi@gmail.com>
2021-04-16 17:24:45 +0800

    feat: scheduler default config
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ scheduler/config/config_darwin.go
@@ -0,0 +1,44 @@
+// +build darwin
+
+package config
+
+import (
+	"runtime"
+
+	"d7y.io/dragonfly/v2/pkg/basic"
+)
+
+var (
+	SchedulerConfigPath = basic.HomeDir + "/.dragonfly/scheduler.yaml"
+)
+
+var SchedulerConfig = Config{
+	Debug:   false,
+	Console: false,
+	Server: serverConfig{
+		Port: 8002,
+	},
+	Worker: schedulerWorkerConfig{
+		WorkerNum:         runtime.GOMAXPROCS(0),
+		WorkerJobPoolSize: 10000,
+		SenderNum:         10,
+		SenderJobPoolSize: 10000,
+	},
+	Scheduler: schedulerConfig{
+		ABTest: false,
+	},
+	CDN: cdnConfig{
+		List: [][]CdnServerConfig{
+			{{
+				CdnName:      "cdn",
+				IP:           "127.0.0.1",
+				RpcPort:      8003,
+				DownloadPort: 8001,
+			}},
+		},
+	},
+	GC: gcConfig{
+		TaskDelay:     3600 * 1000,
+		PeerTaskDelay: 3600 * 1000,
+	},
+}
---
+++ scheduler/config/config_linux.go
@@ -0,0 +1,40 @@
+// +build linux
+
+package config
+
+import "runtime"
+
+var (
+	PeerHostConfigPath = "/etc/dragonfly/scheduler.yaml"
+)
+
+var SchedulerConfig = Config{
+	Debug:   false,
+	Console: false,
+	Server: serverConfig{
+		Port: 8002,
+	},
+	Worker: schedulerWorkerConfig{
+		WorkerNum:         runtime.GOMAXPROCS(0),
+		WorkerJobPoolSize: 10000,
+		SenderNum:         10,
+		SenderJobPoolSize: 10000,
+	},
+	Scheduler: schedulerConfig{
+		ABTest: false,
+	},
+	CDN: cdnConfig{
+		List: [][]CdnServerConfig{
+			{{
+				CdnName:      "cdn",
+				IP:           "127.0.0.1",
+				RpcPort:      8003,
+				DownloadPort: 8001,
+			}},
+		},
+	},
+	GC: gcConfig{
+		TaskDelay:     3600 * 1000,
+		PeerTaskDelay: 3600 * 1000,
+	},
+}
