397cbbc0f0bcc51e1cf89ab2c52848fd25a0be39
Jim Ma <majinjing3@gmail.com>
2021-04-23 11:19:43 +0800

    Fix version output typo

--- version/version.go
+++ version/version.go
@@ -2,6 +2,7 @@ package version
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,13 +10,12 @@ var version = "0.0.0"
 var revision = "0.0.0"
 var buildDate = "00000000-00:00:00"
 
-
 var VersionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of dragonfly",
 	Long:  `Print the version number of dragonfly`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("version: %s \nreversion: %s \nbuildDate: %s\n",
+		fmt.Printf("version: %s \nrevision: %s \nbuildDate: %s\n",
 			version, revision, buildDate)
 	},
 }
