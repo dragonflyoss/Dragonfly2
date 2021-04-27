1c7b98f1ca2b07deccc98a103414b72bef6df33e
Gaius <gaius.qi@gmail.com>
2021-04-27 17:52:21 +0800

    feat: move Dockerfile to deploy directory
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -7,22 +7,23 @@ set -o pipefail
 D7Y_VERSION=${D7Y_VERSION:-"latest"}
 D7Y_REGISTRY=${D7Y_REGISTRY:-d7yio}
 curDir=$(cd "$(dirname "$0")" && pwd)
+IMAGES_DIR="build/images"
 cd "${curDir}/../" || return
 
 docker-build::build-cdn() {
-    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/cdn:"${D7Y_VERSION}" -f Dockerfile.cdn .
+    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/cdn:"${D7Y_VERSION}" -f "${IMAGES_DIR}/cdn/Dockerfile" .
 }
 
 docker-build::build-dfdaemon() {
-    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/dfdaemon:"${D7Y_VERSION}" -f Dockerfile.dfdaemon .
+    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/dfdaemon:"${D7Y_VERSION}" -f "${IMAGES_DIR}/dfdaemon/Dockerfile" .
 }
 
 docker-build::build-scheduler() {
-    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/scheduler:"${D7Y_VERSION}" -f Dockerfile.scheduler .
+    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/scheduler:"${D7Y_VERSION}" -f "${IMAGES_DIR}/scheduler/Dockerfile" .
 }
 
 docker-build::build-manager() {
-    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/manager:"${D7Y_VERSION}" -f Dockerfile.manager .
+    docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/manager:"${D7Y_VERSION}" -f "${IMAGES_DIR}/manager/Dockerfile" .
 }
 
 main() {
