f7b9ebdd9c85f79e08a70e1375e71b891665267f
santong <weipeng.swp@alibaba-inc.com>
2021-05-17 16:13:01 +0800

    feat: scheduler dynconfig log

--- scheduler/config/dynconfig.go
+++ scheduler/config/dynconfig.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"time"
 
+	logger "d7y.io/dragonfly/v2/pkg/dflog"
 	dc "d7y.io/dragonfly/v2/pkg/dynconfig"
 	"d7y.io/dragonfly/v2/pkg/rpc/manager"
 	"d7y.io/dragonfly/v2/pkg/rpc/manager/client"
@@ -65,6 +66,7 @@ func (mc *managerClient) Get() (interface{}, error) {
 		HostName: iputils.HostName,
 		Type:     manager.ResourceType_Scheduler,
 	})
+	logger.Debugf("scheduler config from manager is: %v", scConfig)
 	if err != nil {
 		return nil, err
 	}
