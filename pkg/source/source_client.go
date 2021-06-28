e1ca3d231e796cf32d4e19ee52fa935f7e0a223b
Jim Ma <majinjing3@gmail.com>
2021-06-28 11:31:59 +0800

    fix: skip link for mockgen import
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- pkg/source/source_client.go
+++ pkg/source/source_client.go
@@ -27,7 +27,7 @@ import (
 	"time"
 
 	logger "d7y.io/dragonfly/v2/internal/dflog"
-	_ "github.com/golang/mock/mockgen/model"
+	_ "github.com/golang/mock/mockgen/model" //lint:ignore
 )
 
 var _ ResourceClient = (*ClientManagerImpl)(nil)
