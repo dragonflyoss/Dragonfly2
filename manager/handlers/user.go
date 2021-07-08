22ce13f186e11d1a667d8c2bf0fdf32861546a22
yxxhero <11087727+yxxhero@users.noreply.github.com>
2021-07-08 20:56:40 +0800

    feat: Manager user logic (#419)
    * add unittest for GenerateRandomSalt func
    * add bcrypt package for user logic.
    * add model.User into migrate
    * update jwt middleware func
    * remove encrypted_password export in json
    
    Signed-off-by: yxxhero <aiopsclub@163.com>

---
+++ manager/handlers/user.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"net/http"
+
+	"d7y.io/dragonfly/v2/manager/types"
+	"github.com/gin-gonic/gin"
+)
+
+// @Summary Register user
+// @Description Register user by json config
+// @Tags User
+// @Accept json
+// @Produce json
+// @Param User body types.RegisterRequest true "User"
+// @Success 200 {object} model.User
+// @Failure 400 {object} HTTPError
+// @Failure 500 {object} HTTPError
+// @Router /auth/register [post]
+func (h *Handlers) Register(ctx *gin.Context) {
+	var json types.RegisterRequest
+	if err := ctx.ShouldBindJSON(&json); err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"errors": err.Error()})
+		return
+	}
+
+	userInfo, err := h.Service.Register(json)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, userInfo)
+}
