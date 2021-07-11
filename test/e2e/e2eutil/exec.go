0ab7f5e92e53b1bae08f0909308a546ca4253779
Jim Ma <majinjing3@gmail.com>
2021-07-11 10:42:30 +0800

    test: add dfget e2e test
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- test/e2e/e2eutil/exec.go
+++ test/e2e/e2eutil/exec.go
@@ -2,8 +2,12 @@ package e2eutil
 
 import "os/exec"
 
+const (
+	kindDockerContainer = "kind-control-plane"
+)
+
 func DockerCommand(arg ...string) *exec.Cmd {
-	extArgs := []string{"exec", "-i", "kind-control-plane"}
+	extArgs := []string{"exec", "-i", kindDockerContainer}
 	extArgs = append(extArgs, arg...)
 	return exec.Command("docker", extArgs...)
 }
@@ -16,4 +20,22 @@ func CriCtlCommand(arg ...string) *exec.Cmd {
 
 func KubeCtlCommand(arg ...string) *exec.Cmd {
 	return exec.Command("kubectl", arg...)
+}
+
+type PodExec struct {
+	namespace string
+	name      string
+}
+
+func NewPodExec(namespace string, name string) *PodExec {
+	return &PodExec{
+		namespace: namespace,
+		name:      name,
+	}
+}
+
+func (p *PodExec) Command(arg ...string) *exec.Cmd {
+	extArgs := []string{"-n", p.namespace, "exec", p.name, "--"}
+	extArgs = append(extArgs, arg...)
+	return KubeCtlCommand(extArgs...)
 }
