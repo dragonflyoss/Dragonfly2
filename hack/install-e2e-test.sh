cbd74cf238fa9568f2aa2e899c33a18406dfb490
Gaius <gaius.qi@gmail.com>
2021-07-12 11:24:00 +0800

    feat: test
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hack/install-e2e-test.sh
+++ hack/install-e2e-test.sh
@@ -16,7 +16,6 @@ install-kind() {
       kind create cluster --config ${KIND_CONFIG_PATH}
   else 
       go install sigs.k8s.io/kind@v0.11.1
-      print_step_info "install kind successed"
       print_step_info "wait for kind create cluster"
       kind create cluster --config ${KIND_CONFIG_PATH}
   fi
@@ -30,7 +29,6 @@ install-helm() {
       curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
       chmod 700 get_helm.sh
       ./get_helm.sh
-      print_step_info "install helm success"
       print_step_info "wait for helm install dragonfly"
       helm install --wait --timeout 10m --create-namespace --namespace ${NAMESPACE} dragonfly ${CHARTS_PATH}
   fi
@@ -41,48 +39,38 @@ install-ginkgo() {
       print_step_info "ginkgo has been installed"
   else
       go get -u github.com/onsi/ginkgo/ginkgo
-      print_step_info "install ginkgo success"
   fi
 }
 
 install-local() {
   print_step_info "start kind create cluster"
   install-kind
-  print_step_info "kind create cluster successed"
 
   print_step_info "start building docker images"
   make docker-build
-  print_step_info "build docker images successed"
 
   print_step_info "start loading image for kind"
   make kind-load
-  print_step_info "load docker images successed"
 
   print_step_info "start helm install dragonfly"
   install-helm
-  print_step_info "helm install dragonfly successed"
 
   print_step_info "start ginkgo install dragonfly"
   install-ginkgo
-  print_step_info "ginkgo install dragonfly successed"
 }
 
 install-actions() {
   print_step_info "start building docker images"
   make docker-build
-  print_step_info "build docker images successed"
 
   print_step_info "start loading image for kind"
   make kind-load
-  print_step_info "load docker images successed"
 
   print_step_info "start helm install dragonfly"
   install-helm
-  print_step_info "helm install dragonfly successed"
 
   print_step_info "start ginkgo install dragonfly"
   install-ginkgo
-  print_step_info "ginkgo install dragonfly successed"
 }
 
 print_step_info() {
