5c3989ebe0526a100834e8cfb5e2099dcf112a01
Gaius <gaius.qi@gmail.com>
2021-07-12 11:13:19 +0800

    feat: print_info
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hack/install-e2e-test.sh
+++ hack/install-e2e-test.sh
@@ -12,77 +12,83 @@ cd "${curDir}/../" || return
 
 install-kind() {
   if which kind >/dev/null ; then
-      echo "wait for kind create cluster"
+      print_step_info "wait for kind create cluster"
       kind create cluster --config ${KIND_CONFIG_PATH}
   else 
       go install sigs.k8s.io/kind@v0.11.1
-      echo "install kind successed"
-      echo "wait for kind create cluster"
+      print_step_info "install kind successed"
+      print_step_info "wait for kind create cluster"
       kind create cluster --config ${KIND_CONFIG_PATH}
   fi
 }
 
 install-helm() {
   if which helm >/dev/null ; then
-      echo "wait for helm install dragonfly"
+      print_step_info "wait for helm install dragonfly"
       helm install --wait --timeout 10m --create-namespace --namespace ${NAMESPACE} dragonfly ${CHARTS_PATH}
   else
       curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
       chmod 700 get_helm.sh
       ./get_helm.sh
-      echo "install helm success"
-      echo "wait for helm install dragonfly"
+      print_step_info "install helm success"
+      print_step_info "wait for helm install dragonfly"
       helm install --wait --timeout 10m --create-namespace --namespace ${NAMESPACE} dragonfly ${CHARTS_PATH}
   fi
 }
 
 install-ginkgo() {
   if which ginkgo >/dev/null ; then
-      echo "ginkgo has been installed"
+      print_step_info "ginkgo has been installed"
   else
       go get -u github.com/onsi/ginkgo/ginkgo
-      echo "install ginkgo success"
+      print_step_info "install ginkgo success"
   fi
 }
 
 install-local() {
-  echo "start kind create cluster"
+  print_step_info "start kind create cluster"
   install-kind
-  echo "kind create cluster successed"
+  print_step_info "kind create cluster successed"
 
-  echo "start building docker images"
+  print_step_info "start building docker images"
   make docker-build
-  echo "build docker images successed"
+  print_step_info "build docker images successed"
 
-  echo "start loading image for kind"
+  print_step_info "start loading image for kind"
   make kind-load
-  echo "load docker images successed"
+  print_step_info "load docker images successed"
 
-  echo "start helm install dragonfly"
+  print_step_info "start helm install dragonfly"
   install-helm
-  echo "helm install dragonfly successed"
+  print_step_info "helm install dragonfly successed"
 
-  echo "start ginkgo install dragonfly"
+  print_step_info "start ginkgo install dragonfly"
   install-ginkgo
-  echo "ginkgo install dragonfly successed"
+  print_step_info "ginkgo install dragonfly successed"
 }
 
 install-actions() {
-  echo "start building docker images"
+  print_step_info "start building docker images"
   make docker-build
-  echo "build docker images successed"
+  print_step_info "build docker images successed"
 
-  echo "start loading image for kind"
+  print_step_info "start loading image for kind"
   make kind-load
-  echo "load docker images successed"
+  print_step_info "load docker images successed"
 
-  echo "start helm install dragonfly"
+  print_step_info "start helm install dragonfly"
   install-helm
-  echo "helm install dragonfly successed"
+  print_step_info "helm install dragonfly successed"
 
-  echo "start ginkgo install dragonfly"
+  print_step_info "start ginkgo install dragonfly"
   install-ginkgo
-  echo "ginkgo install dragonfly successed"
+  print_step_info "ginkgo install dragonfly successed"
+}
+
+print_step_info() {
+  echo "============================="
+  echo $1
+  echo "============================="
 }
 
 main() {
