23e190333746209ec584eb703b27a560676b4271
lengrongfu <lengrongfu@meituan.com>
2021-07-22 14:20:03 +0800

    fix:GOPROXY var
    Signed-off-by: lengrongfu <lengrongfu@meituan.com>

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -9,6 +9,7 @@ D7Y_REGISTRY=${D7Y_REGISTRY:-d7yio}
 curDir=$(cd "$(dirname "$0")" && pwd)
 IMAGES_DIR="build/images"
 cd "${curDir}/../" || return
+GOPROXY=${GOPROXY:-`go env GOPROXY`}
 
 docker-build::build-cdn() {
     docker build --build-arg GOPROXY="${GOPROXY}" -t "${D7Y_REGISTRY}"/cdn:"${D7Y_VERSION}" -f "${IMAGES_DIR}/cdn/Dockerfile" .
