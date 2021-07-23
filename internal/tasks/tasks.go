64d1e8555738c8b259622be9f36797008702811b
张哲溢 <zhangzheyi@zhangzheyideMacBook-Pro.local>
2021-07-23 10:50:57 +0800

    feat: add func SendTask to struct Tasks
    Signed-off-by: 张哲溢 <zhangzheyi@zhangzheyideMacBook-Pro.local>

--- internal/tasks/tasks.go
+++ internal/tasks/tasks.go
@@ -1,7 +1,10 @@
 package tasks
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/RichardKnop/machinery/v1/backends/result"
+	machineryv1tasks "github.com/RichardKnop/machinery/v1/tasks"
 
 	"github.com/RichardKnop/machinery/v1"
 	machineryv1config "github.com/RichardKnop/machinery/v1/config"
@@ -53,4 +56,21 @@ func New(cfg *Config, queue Queue) (*Tasks, error) {
 
 func (t *Tasks) LaunchWorker(consumerTag string, concurrency int) error {
 	return t.Server.NewWorker(consumerTag, concurrency).Launch()
+}
+
+func (t *Tasks) SendTask(taskName string, taskArg interface{}) (*result.AsyncResult, error) {
+	argJson, err := json.Marshal(taskArg)
+	if err != nil {
+		return nil, err
+	}
+	signature := &machineryv1tasks.Signature{
+		Name: taskName,
+		Args: []machineryv1tasks.Arg{
+			{
+				Type:  "string",
+				Value: string(argJson),
+			},
+		},
+	}
+	return t.Server.SendTask(signature)
 }
