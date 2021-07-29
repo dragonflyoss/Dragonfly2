9ac80875b7f6c7c38fbabf76eddd507d96bae43d
zzy987 <67889264+zzy987@users.noreply.github.com>
2021-07-29 13:36:12 +0800

    chore: add logs for task in scheduler
    Signed-off-by: zzy987 <67889264+zzy987@users.noreply.github.com>

--- scheduler/task/task.go
+++ scheduler/task/task.go
@@ -12,7 +12,6 @@ import (
 	"d7y.io/dragonfly/v2/scheduler/config"
 	"d7y.io/dragonfly/v2/scheduler/core"
 	"d7y.io/dragonfly/v2/scheduler/types"
-	"fmt"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 	"strings"
@@ -42,18 +41,22 @@ func New(ctx context.Context, cfg *config.TaskConfig, hostname string, service *
 	}
 	globalTask, err := internaltasks.New(redisConfig, internaltasks.GlobalQueue)
 	if err != nil {
+		logger.Errorf("create global task queue error: %v", err)
 		return nil, err
 	}
 	schedulerTask, err := internaltasks.New(redisConfig, internaltasks.SchedulersQueue)
 	if err != nil {
+		logger.Errorf("create scheduler task queue error: %v", err)
 		return nil, err
 	}
 	localQueue, err := internaltasks.GetSchedulerQueue(hostname)
 	if err != nil {
+		logger.Errorf("get local task queue name error: %v", err)
 		return nil, err
 	}
 	localTask, err := internaltasks.New(redisConfig, localQueue)
 	if err != nil {
+		logger.Errorf("create local task queue error: %v", err)
 		return nil, err
 	}
 
@@ -67,33 +70,47 @@ func New(ctx context.Context, cfg *config.TaskConfig, hostname string, service *
 	}
 	err = globalTask.RegisterTask(internaltasks.PreheatTask, t.preheat)
 	if err != nil {
+		logger.Errorf("register preheat task to global queue error: %v", err)
 		return nil, err
 	}
 	err = schedulerTask.RegisterTask(internaltasks.PreheatTask, t.preheat)
 	if err != nil {
+		logger.Errorf("register preheat task to scheduler queue error: %v", err)
 		return nil, err
 	}
 	err = localTask.RegisterTask(internaltasks.PreheatTask, t.preheat)
 	if err != nil {
+		logger.Errorf("register preheat task to local queue error: %v", err)
 		return nil, err
 	}
 
 	return t, nil
 }
 
-// TODO: add log
 func (t *task) Serve() error {
 	g := errgroup.Group{}
 	g.Go(func() error {
+		logger.Debugf("ready to launch %d worker(s) on global queue", t.cfg.GlobalWorkerNum)
 		err := t.globalTasks.LaunchWorker("global_worker", t.cfg.GlobalWorkerNum)
+		if err != nil {
+			logger.Errorf("global queue worker error: %v", err)
+		}
 		return err
 	})
 	g.Go(func() error {
+		logger.Debugf("ready to launch %d worker(s) on scheduler queue", t.cfg.SchedulerWorkerNum)
 		err := t.schedulerTasks.LaunchWorker("scheduler_worker", t.cfg.SchedulerWorkerNum)
+		if err != nil {
+			logger.Errorf("scheduler queue worker error: %v", err)
+		}
 		return err
 	})
 	g.Go(func() error {
+		logger.Debugf("ready to launch %d worker(s) on local queue", t.cfg.LocalWorkerNum)
 		err := t.localTasks.LaunchWorker("local_worker", t.cfg.LocalWorkerNum)
+		if err != nil {
+			logger.Errorf("local queue worker error: %v", err)
+		}
 		return err
 	})
 	return g.Wait()
@@ -103,15 +120,16 @@ func (t *task) preheat(req string) (string, error) {
 	request := &internaltasks.PreheatRequest{}
 	err := internaltasks.UnmarshalRequest(req, request)
 	if err != nil {
+		logger.Errorf("unmarshal request err: %v, request body: %s", err, req)
 		return "", err
 	}
 	if !urlutils.IsValidURL(request.URL) {
+		logger.Errorf("request url \"%s\" is invalid", request.URL)
 		return "", errors.Errorf("invalid url: %s", request.URL)
 	}
 
 	meta := &base.UrlMeta{Header: request.Headers, Tag: request.Tag, Filter: request.Filter}
 	// CDN only support MD5 now.
-	fmt.Println(request.Digest)
 	if strings.HasPrefix(request.Digest, "md5") {
 		meta.Digest = request.Digest
 	}
@@ -119,6 +137,7 @@ func (t *task) preheat(req string) (string, error) {
 		meta.Range = rg
 	}
 	taskID := idgen.TaskID(request.URL, request.Filter, meta, request.Tag)
+	logger.Debugf("ready to preheat \"%s\", taskID = %s", request.URL, taskID)
 	task := types.NewTask(taskID, request.URL, request.Filter, request.Tag, meta)
 	task, err = t.service.GetOrCreateTask(t.ctx, task)
 	if err != nil {
