5b579fae3550c85bb025d3a0a76d8b954bbbc602
Gaius <gaius.qi@gmail.com>
2021-07-29 20:11:25 +0800

    feat: format
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- internal/tasks/types.go
+++ internal/tasks/types.go
@@ -2,13 +2,12 @@ package tasks
 
 //TODO: check if required or omitempty
 type PreheatRequest struct {
-	URL      string            `json:"url" binding:"required"`
-	Tag      string            `json:"tag" binding:"required"`
-	Digest   string            `json:"digest" binding:"omitempty"`
-	Filter   string            `json:"filter" binding:"omitempty"`
-	Headers  map[string]string `json:"headers" binding:"omitempty"`
+	URL     string            `json:"url" binding:"required"`
+	Tag     string            `json:"tag" binding:"required"`
+	Digest  string            `json:"digest" binding:"omitempty"`
+	Filter  string            `json:"filter" binding:"omitempty"`
+	Headers map[string]string `json:"headers" binding:"omitempty"`
 }
 
-type PreheatResponse struct{
-
+type PreheatResponse struct {
 }
