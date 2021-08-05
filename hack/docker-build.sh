83f017ead662cf91fe26be75e0a4aaa10ae5e11f
Jim Ma <majinjing3@gmail.com>
2021-08-05 10:56:18 +0800

    chore: optimize docker-build.sh
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -4,69 +4,49 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+curDir=$(cd "$(dirname "$0")" && pwd)
+cd "${curDir}/../" || return
+
 D7Y_VERSION=${D7Y_VERSION:-"latest"}
 D7Y_REGISTRY=${D7Y_REGISTRY:-d7yio}
-curDir=$(cd "$(dirname "$0")" && pwd)
 IMAGES_DIR="build/images"
 BASE_IMAGE=${BASE_IMAGE:-alpine:3.12}
-cd "${curDir}/../" || return
+
 GOPROXY=${GOPROXY:-`go env GOPROXY`}
 GOTAGS=${GOTAGS:-}
 GOGCFLAGS=${GOGCFLAGS:-}
 
-docker-build::build-cdn() {
-    docker build \
-      --build-arg GOPROXY="${GOPROXY}" \
-      --build-arg GOTAGS="${GOTAGS}" \
-      --build-arg GOGCFLAGS="${GOGCFLAGS}" \
-      --build-arg BASE_IMAGE="${BASE_IMAGE}" \
-      -t "${D7Y_REGISTRY}"/cdn:"${D7Y_VERSION}" \
-      -f "${IMAGES_DIR}/cdn/Dockerfile" .
-}
+# enable bash debug output
+DEBUG=${DEBUG:-}
 
-docker-build::build-dfdaemon() {
-    docker build \
-      --build-arg GOPROXY="${GOPROXY}" \
-      --build-arg GOTAGS="${GOTAGS}" \
-      --build-arg GOGCFLAGS="${GOGCFLAGS}" \
-      --build-arg BASE_IMAGE="${BASE_IMAGE}" \
-      -t "${D7Y_REGISTRY}"/dfdaemon:"${D7Y_VERSION}" \
-      -f "${IMAGES_DIR}/dfdaemon/Dockerfile" .
-}
+if [[ -n "$DEBUG" ]]; then
+    set -x
+fi
 
-docker-build::build-scheduler() {
+docker-build() {
+    name=$1
     docker build \
       --build-arg GOPROXY="${GOPROXY}" \
       --build-arg GOTAGS="${GOTAGS}" \
       --build-arg GOGCFLAGS="${GOGCFLAGS}" \
       --build-arg BASE_IMAGE="${BASE_IMAGE}" \
-      -t "${D7Y_REGISTRY}"/scheduler:"${D7Y_VERSION}" \
-      -f "${IMAGES_DIR}/scheduler/Dockerfile" .
-}
-
-docker-build::build-manager() {
-    docker build \
-      --build-arg GOPROXY="${GOPROXY}" \
-      --build-arg GOTAGS="${GOTAGS}" \
-      --build-arg GOGCFLAGS="${GOGCFLAGS}" \
-      --build-arg BASE_IMAGE="${BASE_IMAGE}" \
-      -t "${D7Y_REGISTRY}"/manager:"${D7Y_VERSION}" \
-      -f "${IMAGES_DIR}/manager/Dockerfile" .
+      -t "${D7Y_REGISTRY}/${name}:${D7Y_VERSION}" \
+      -f "${IMAGES_DIR}/${name}/Dockerfile" .
 }
 
 main() {
     case "${1-}" in
     cdn)
-        docker-build::build-cdn
+        docker-build cdn
         ;;
     dfdaemon)
-        docker-build::build-dfdaemon
+        docker-build dfdaemon
         ;;
     scheduler)
-        docker-build::build-scheduler
+        docker-build scheduler
         ;;
     manager)
-        docker-build::build-manager
+        docker-build manager
     esac
 }
 
