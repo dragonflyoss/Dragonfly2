85bfa3635a54f5615924a15b85429f50387cc7a2
Jim Ma <majinjing3@gmail.com>
2021-08-06 10:21:15 +0800

    feat: back source when no available peers or scheduler error (#521)
    * feature: back source when no available peers
    
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- client/daemon/peer/peertask_file_test.go
+++ client/daemon/peer/peertask_file_test.go
@@ -61,7 +61,14 @@ func TestFilePeerTask_BackSource_WithContentLength(t *testing.T) {
 	)
 	defer os.Remove(output)
 
-	schedulerClient, storageManager := setupPeerTaskManagerComponents(ctrl, taskID, int64(mockContentLength), int32(pieceSize), pieceParallelCount)
+	schedulerClient, storageManager := setupPeerTaskManagerComponents(
+		ctrl,
+		componentsOption{
+			taskID:             taskID,
+			contentLength:      int64(mockContentLength),
+			pieceSize:          int32(pieceSize),
+			pieceParallelCount: pieceParallelCount,
+		})
 	defer storageManager.CleanUp()
 
 	downloader := NewMockPieceDownloader(ctrl)
@@ -121,11 +128,12 @@ func TestFilePeerTask_BackSource_WithContentLength(t *testing.T) {
 		ptm.schedulerOption,
 		0)
 	assert.Nil(err, "new file peer task")
-	pt.(*filePeerTask).backSource = true
+	pt.needBackSource = true
 
 	pt.SetCallback(&filePeerTaskCallback{
 		ctx:   ctx,
 		ptm:   ptm,
+		pt:    pt,
 		req:   req,
 		start: time.Now(),
 	})
@@ -171,7 +179,14 @@ func TestFilePeerTask_BackSource_WithoutContentLength(t *testing.T) {
 	)
 	defer os.Remove(output)
 
-	schedulerClient, storageManager := setupPeerTaskManagerComponents(ctrl, taskID, int64(mockContentLength), int32(pieceSize), pieceParallelCount)
+	schedulerClient, storageManager := setupPeerTaskManagerComponents(
+		ctrl,
+		componentsOption{
+			taskID:             taskID,
+			contentLength:      int64(mockContentLength),
+			pieceSize:          int32(pieceSize),
+			pieceParallelCount: pieceParallelCount,
+		})
 	defer storageManager.CleanUp()
 
 	downloader := NewMockPieceDownloader(ctrl)
@@ -232,11 +247,12 @@ func TestFilePeerTask_BackSource_WithoutContentLength(t *testing.T) {
 		ptm.schedulerOption,
 		0)
 	assert.Nil(err, "new file peer task")
-	pt.(*filePeerTask).backSource = true
+	pt.needBackSource = true
 
 	pt.SetCallback(&filePeerTaskCallback{
 		ctx:   ctx,
 		ptm:   ptm,
+		pt:    pt,
 		req:   req,
 		start: time.Now(),
 	})
