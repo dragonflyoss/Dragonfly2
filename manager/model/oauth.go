608323415785438b95ebc0f6bde9038e5aa2ebd1
yxxhero <aiopsclub@163.com>
2021-08-11 16:36:39 +0800

    fix lint error
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- manager/model/oauth.go
+++ manager/model/oauth.go
@@ -6,8 +6,8 @@ type Oauth struct {
 	ClientID     string `gorm:"column:client_id;type:varchar(256);index:unique;not null;comment:client id for oauth" json:"client_id"`
 	ClientSecret string `gorm:"column:client_secret;type:varchar(1024);comment:client secret for oauth" json:"client_secret"`
 	// scope list split by ','
-	Scopes   string `gorm:"column:scopes;type:varchar(1024);comment:scopes" json:"scopes"`
-	UserInfoURL  string `gorm:"column:user_info_url;type:varchar(256);not null;comment:user info url" json:"user_info_url"`
-	AuthURL  string `gorm:"column:auth_url;type:varchar(256);not null;comment:auth url" json:"auth_url"`
-	TokenURL string `gorm:"column:token_url;type:varchar(256);not null;comment:token url" json:"token_url"`
+	Scopes      string `gorm:"column:scopes;type:varchar(1024);comment:scopes" json:"scopes"`
+	UserInfoURL string `gorm:"column:user_info_url;type:varchar(256);not null;comment:user info url" json:"user_info_url"`
+	AuthURL     string `gorm:"column:auth_url;type:varchar(256);not null;comment:auth url" json:"auth_url"`
+	TokenURL    string `gorm:"column:token_url;type:varchar(256);not null;comment:token url" json:"token_url"`
 }
