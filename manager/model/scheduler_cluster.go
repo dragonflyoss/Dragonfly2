de6f0858fe2848759daafed98af888ff8c9a03cd
Gaius <gaius.qi@gmail.com>
2021-08-12 13:45:56 +0800

    feat: init id
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- manager/model/scheduler_cluster.go
+++ manager/model/scheduler_cluster.go
@@ -7,7 +7,7 @@ type SchedulerCluster struct {
 	Config          JSONMap       `gorm:"column:config;not null;comment:configuration" json:"config"`
 	ClientConfig    JSONMap       `gorm:"column:client_config;not null;comment:client configuration" json:"client_config"`
 	Scopes          JSONMap       `gorm:"column:scopes;comment:match scopes" json:"scopes"`
-	IsDefault       bool          `gorm:"column:is_default;not null;default:false;comment:default scheduler" json:"is_default"`
+	IsDefault       bool          `gorm:"column:is_default;not null;default:false;comment:default scheduler cluster" json:"is_default"`
 	CDNClusters     []CDNCluster  `gorm:"many2many:cdn_cluster_scheduler_cluster;" json:"-"`
 	Schedulers      []Scheduler   `json:"-"`
 	SecurityGroupID uint          `gorm:"comment:security group id"`
