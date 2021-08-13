5eac47eab9aea3d5fccd5d16a80f7f5f20acf8ba
yxxhero <aiopsclub@163.com>
2021-08-13 08:56:26 +0800

    fix some issue
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- manager/model/settings.go
+++ manager/model/settings.go
@@ -1,7 +1,7 @@
 package model
 
 type Settings struct {
-	ID    uint   `gorm:"primarykey;comment:id" json:"id"`
+	Model
 	Key   string `gorm:"column:key;type:varchar(256);index:uk_settings_key,unique;not null;comment:setting key" json:"key"`
 	Value string `gorm:"column:value;type:varchar(256);not null;comment:setting value" json:"value"`
 }
