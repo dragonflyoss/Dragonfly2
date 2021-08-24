de240478a087be3791584c97e14c8ea356ea0e53
zzy987 <67889264+zzy987@users.noreply.github.com>
2021-08-24 21:17:51 +0800

    chore: put curl in manager
    Signed-off-by: zzy987 <67889264+zzy987@users.noreply.github.com>

--- test/e2e/preheat_test.go
+++ test/e2e/preheat_test.go
@@ -27,6 +27,17 @@ var _ = Describe("Preheat with manager", func() {
 				cdnPods[i] = e2eutil.NewPodExec(dragonflyNamespace, podName, "cdn")
 			}
 
+			var managerPods [3]*e2eutil.PodExec
+			for i := 0; i < 3; i++ {
+				out, err = e2eutil.KubeCtlCommand("-n", dragonflyNamespace, "get", "pod", "-l", "component=manager",
+					"-o", fmt.Sprintf("jsonpath='{range .items[%d]}{.metadata.name}{end}'", i)).CombinedOutput()
+				podName := strings.Trim(string(out), "'")
+				Expect(err).NotTo(HaveOccurred())
+				fmt.Println(podName)
+				Expect(strings.HasPrefix(podName, "dragonfly-manager-")).Should(BeTrue())
+				managerPods[i] = e2eutil.NewPodExec(dragonflyNamespace, podName, "manager")
+			}
+
 			for _, v := range e2eutil.GetFileList() {
 				url := e2eutil.GetFileURL(v)
 				fmt.Println("download url " + url)
@@ -37,12 +48,8 @@ var _ = Describe("Preheat with manager", func() {
 				Expect(err).NotTo(HaveOccurred())
 				//sha256sum1 := strings.Split(string(out), " ")[0]
 
-				out, err = cdnPods[0].Command("kubectl", "get", "services").CombinedOutput()
-				fmt.Println(string(out))
-				Expect(err).NotTo(HaveOccurred())
-
 				// download file
-				out, err = cdnPods[0].Command("curl", "-X", "POST", "-H", `'Content-Type:application/json'`,
+				out, err = managerPods[0].Command("curl", "-X", "POST", "-H", `'Content-Type:application/json'`,
 					"-d", fmt.Sprintf(`'{"type": "file", "url": "%s"}'`, url), "http://dragonfly-manager/preheats").CombinedOutput()
 				fmt.Println(string(out))
 				Expect(err).NotTo(HaveOccurred())
