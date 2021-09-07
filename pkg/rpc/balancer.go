1bc5e58ed37bf15e2f175338e9ba1f8f2366758d
zzy987 <67889264+zzy987@users.noreply.github.com>
2021-09-07 11:45:32 +0800

    feat: refactor the codes about state in the balancer
    Signed-off-by: zzy987 <67889264+zzy987@users.noreply.github.com>

--- pkg/rpc/balancer.go
+++ pkg/rpc/balancer.go
@@ -24,8 +24,9 @@ import (
 	"sync/atomic"
 	"time"
 
-	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
+
+	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/resolver"
 	"k8s.io/client-go/util/workqueue"
@@ -52,13 +53,12 @@ func init() {
 type d7yBalancerBuilder struct{}
 
 // Build creates a d7yBalancer, and starts its scManager.
-func (d *d7yBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
+func (builder *d7yBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
 	b := &d7yBalancer{
 		cc:             cc,
 		addrInfos:      make(map[string]resolver.Address),
 		subConns:       make(map[string]balancer.SubConn),
 		scInfos:        sync.Map{},
-		csEvltr:        &balancer.ConnectivityStateEvaluator{},
 		pickResultChan: make(chan pickResult),
 		pickResults:    workqueue.New(),
 		scCounts:       make(map[balancer.SubConn]*int32),
@@ -68,7 +68,7 @@ func (d *d7yBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOp
 }
 
 // Name returns the name of the d7yBalancer registering in grpc.
-func (d *d7yBalancerBuilder) Name() string {
+func (builder *d7yBalancerBuilder) Name() string {
 	return d7yBalancerPolicy
 }
 
@@ -82,10 +82,7 @@ type subConnInfo struct {
 type d7yBalancer struct {
 	// cc points to the balancer.ClientConn who creates the d7yBalancer.
 	cc balancer.ClientConn
-	//ccCheater sync.Once
 
-	// csEvltr appears in baseBalancer, I don't know much about it.
-	csEvltr *balancer.ConnectivityStateEvaluator
 	// state indicates the state of the whole ClientConn from the perspective of the balancer.
 	state connectivity.State
 
@@ -116,21 +113,21 @@ type d7yBalancer struct {
 
 // UpdateClientConnState is implemented from balancer.Balancer, modified from the baseBalancer,
 // ClientConn will call it after Builder builds the balancer to pass the necessary data.
-func (c *d7yBalancer) UpdateClientConnState(s balancer.ClientConnState) error {
-	c.resolverErr = nil
+func (b *d7yBalancer) UpdateClientConnState(s balancer.ClientConnState) error {
+	b.resolverErr = nil
 	addrsSet := make(map[string]struct{})
 	for _, a := range s.ResolverState.Addresses {
 		addr := a.Addr
-		c.addrInfos[addr] = a
+		b.addrInfos[addr] = a
 		addrsSet[addr] = struct{}{}
-		if sc, ok := c.subConns[addr]; !ok {
-			newSC, err := c.cc.NewSubConn([]resolver.Address{a}, balancer.NewSubConnOptions{HealthCheckEnabled: false})
+		if sc, ok := b.subConns[addr]; !ok {
+			newSC, err := b.cc.NewSubConn([]resolver.Address{a}, balancer.NewSubConnOptions{HealthCheckEnabled: false})
 			if err != nil {
 				log.Printf("d7yBalancer: failed to create new SubConn: %v", err)
 				continue
 			}
-			c.subConns[addr] = newSC
-			c.scInfos.Store(newSC, &subConnInfo{
+			b.subConns[addr] = newSC
+			b.scInfos.Store(newSC, &subConnInfo{
 				state: connectivity.Idle,
 				addr:  addr,
 			})
@@ -149,17 +146,17 @@ func (c *d7yBalancer) UpdateClientConnState(s balancer.ClientConnState) error {
 			// We should use another way to achieve ClientConn.UpdateState() with a picker before the first pick,
 			// for example, we call ClientConn.UpdateState() directly.
 		} else {
-			c.cc.UpdateAddresses(sc, []resolver.Address{a})
+			b.cc.UpdateAddresses(sc, []resolver.Address{a})
 		}
 	}
-	for a, sc := range c.subConns {
+	for a, sc := range b.subConns {
 		if _, ok := addrsSet[a]; !ok {
-			c.cc.RemoveSubConn(sc)
-			delete(c.subConns, a)
+			b.cc.RemoveSubConn(sc)
+			delete(b.subConns, a)
 		}
 	}
 	if len(s.ResolverState.Addresses) == 0 {
-		c.ResolverError(fmt.Errorf("produced zero addresses"))
+		b.ResolverError(fmt.Errorf("produced zero addresses"))
 		return balancer.ErrBadResolverState
 	}
 
@@ -167,67 +164,66 @@ func (c *d7yBalancer) UpdateClientConnState(s balancer.ClientConnState) error {
 	// Connection has not been ready yet. The next two lines is a trick aims to make grpc continue executing.
 	// if the picker is nil in ClientConn, it will block at next pick in pickerWrapper,
 	// So we have to generate the picker first.
-	c.regeneratePicker()
+	b.regeneratePicker()
 	// we call ClientConn.UpdateState() directly.
-	c.cc.UpdateState(balancer.State{ConnectivityState: connectivity.Ready, Picker: c.picker})
+	b.cc.UpdateState(balancer.State{ConnectivityState: b.state, Picker: b.picker})
 
 	return nil
 }
 
 // ResolverError is implemented from balancer.Balancer, copied from baseBalancer.
-func (c *d7yBalancer) ResolverError(err error) {
-	c.resolverErr = err
-	if len(c.subConns) == 0 {
-		c.state = connectivity.TransientFailure
-	}
+func (b *d7yBalancer) ResolverError(err error) {
+	b.resolverErr = err
 
-	if c.state != connectivity.TransientFailure {
+	b.regeneratePicker()
+	if b.state != connectivity.TransientFailure {
 		// The picker will not change since the balancer does not currently
 		// report an error.
 		return
 	}
-	c.regeneratePicker()
-	c.cc.UpdateState(balancer.State{
-		ConnectivityState: c.state,
-		Picker:            c.picker,
+	b.cc.UpdateState(balancer.State{
+		ConnectivityState: b.state,
+		Picker:            b.picker,
 	})
 }
 
 // regeneratePicker generates a new picker to replace the old one with new data.
-func (c *d7yBalancer) regeneratePicker() {
-	if c.state == connectivity.TransientFailure {
-		c.picker = base.NewErrPicker(c.mergeErrors())
-		return
-	}
-	readySCs := make(map[string]balancer.SubConn)
-	for addr, sc := range c.subConns {
+func (b *d7yBalancer) regeneratePicker() {
+	availableSCs := make(map[string]balancer.SubConn)
+	for addr, sc := range b.subConns {
 		// The next line may not be safe, but we have to use subConns without check,
 		// for we have not set up connections with any SubConn, all of them are Idle.
-		if st, ok := c.scInfos.Load(sc); ok {
+		if st, ok := b.scInfos.Load(sc); ok {
 			info := st.(*subConnInfo)
 			if info.state != connectivity.Shutdown && info.state != connectivity.TransientFailure {
-				readySCs[addr] = sc
+				availableSCs[addr] = sc
 			}
 		}
 	}
-	c.picker = NewD7yReportingPicker(readySCs, c.pickResultChan)
+	if len(availableSCs) == 0 {
+		b.state = connectivity.TransientFailure
+		b.picker = base.NewErrPicker(b.mergeErrors())
+	} else {
+		b.state = connectivity.Ready
+		b.picker = NewD7yReportingPicker(availableSCs, b.pickResultChan)
+	}
 }
 
 // mergeErrors is copied from baseBalancer.
-func (c *d7yBalancer) mergeErrors() error {
-	if c.connErr == nil {
-		return fmt.Errorf("last resolver error: %v", c.resolverErr)
+func (b *d7yBalancer) mergeErrors() error {
+	if b.connErr == nil {
+		return fmt.Errorf("last resolver error: %v", b.resolverErr)
 	}
-	if c.resolverErr == nil {
-		return fmt.Errorf("last connection error: %v", c.connErr)
+	if b.resolverErr == nil {
+		return fmt.Errorf("last connection error: %v", b.connErr)
 	}
-	return fmt.Errorf("last connection error: %v; last resolver error: %v", c.connErr, c.resolverErr)
+	return fmt.Errorf("last connection error: %v; last resolver error: %v", b.connErr, b.resolverErr)
 }
 
 // UpdateSubConnState is implemented by balancer.Balancer, modified from baseBalancer
-func (c *d7yBalancer) UpdateSubConnState(sc balancer.SubConn, state balancer.SubConnState) {
+func (b *d7yBalancer) UpdateSubConnState(sc balancer.SubConn, state balancer.SubConnState) {
 	s := state.ConnectivityState
-	v, ok := c.scInfos.Load(sc)
+	v, ok := b.scInfos.Load(sc)
 	if !ok {
 		return
 	}
@@ -241,41 +237,38 @@ func (c *d7yBalancer) UpdateSubConnState(sc balancer.SubConn, state balancer.Sub
 	switch s {
 	case connectivity.Idle:
 		// I do not know when it will come here.
-		sc.Connect()
+		// sc.Connect()
 	case connectivity.Shutdown:
-		c.scInfos.Delete(sc)
+		b.scInfos.Delete(sc)
 	case connectivity.TransientFailure:
-		c.connErr = state.ConnectionError
+		b.connErr = state.ConnectionError
 	}
 
-	c.state = c.csEvltr.RecordTransition(oldS, s)
-
-	// It seems like this if expression can be removed.
-	if (s == connectivity.Ready) != (oldS == connectivity.Ready) ||
-		c.state == connectivity.TransientFailure {
-		c.regeneratePicker()
+	if (s == connectivity.TransientFailure || s == connectivity.Shutdown) != (oldS == connectivity.TransientFailure || oldS == connectivity.Shutdown) ||
+		b.state == connectivity.TransientFailure {
+		b.regeneratePicker()
 	}
 
-	c.cc.UpdateState(balancer.State{ConnectivityState: c.state, Picker: c.picker})
+	b.cc.UpdateState(balancer.State{ConnectivityState: b.state, Picker: b.picker})
 }
 
 // Close is implemented by balancer.Balancer, copied from baseBalancer.
-func (c *d7yBalancer) Close() {}
+func (b *d7yBalancer) Close() {}
 
 // resetSubConn will replace a SubConn with a new idle SubConn.
-func (c *d7yBalancer) resetSubConn(sc balancer.SubConn) error {
-	addr, err := c.getSubConnAddr(sc)
+func (b *d7yBalancer) resetSubConn(sc balancer.SubConn) error {
+	addr, err := b.getSubConnAddr(sc)
 	if err != nil {
 		return err
 	}
 	log.Printf("Reset connect with addr %s", addr)
-	err = c.resetSubConnWithAddr(addr)
+	err = b.resetSubConnWithAddr(addr)
 	return err
 }
 
 // getSubConnAddr returns the address string of a SubConn.
-func (c *d7yBalancer) getSubConnAddr(sc balancer.SubConn) (string, error) {
-	v, ok := c.scInfos.Load(sc)
+func (b *d7yBalancer) getSubConnAddr(sc balancer.SubConn) (string, error) {
+	v, ok := b.scInfos.Load(sc)
 	if !ok {
 		return "", SubConnNotFoundError
 	}
@@ -283,51 +276,51 @@ func (c *d7yBalancer) getSubConnAddr(sc balancer.SubConn) (string, error) {
 }
 
 // resetSubConnWithAddr creates a new idle SubConn for the address string, and remove the old one.
-func (c *d7yBalancer) resetSubConnWithAddr(addr string) error {
-	sc, ok := c.subConns[addr]
+func (b *d7yBalancer) resetSubConnWithAddr(addr string) error {
+	sc, ok := b.subConns[addr]
 	if !ok {
 		return SubConnNotFoundError
 	}
-	c.scInfos.Delete(sc)
-	c.cc.RemoveSubConn(sc)
-	newSC, err := c.cc.NewSubConn([]resolver.Address{c.addrInfos[addr]}, balancer.NewSubConnOptions{HealthCheckEnabled: false})
+	b.scInfos.Delete(sc)
+	b.cc.RemoveSubConn(sc)
+	newSC, err := b.cc.NewSubConn([]resolver.Address{b.addrInfos[addr]}, balancer.NewSubConnOptions{HealthCheckEnabled: false})
 	if err != nil {
 		log.Printf("Consistent Hash Balancer: failed to create new SubConn: %v", err)
 		return ResetSubConnFailError
 	}
-	c.subConns[addr] = newSC
-	c.scInfos.Store(newSC, &subConnInfo{
+	b.subConns[addr] = newSC
+	b.scInfos.Store(newSC, &subConnInfo{
 		state: connectivity.Idle,
 		addr:  addr,
 	})
-	c.regeneratePicker()
-	c.cc.UpdateState(balancer.State{ConnectivityState: c.state, Picker: c.picker})
+	b.regeneratePicker()
+	b.cc.UpdateState(balancer.State{ConnectivityState: b.state, Picker: b.picker})
 	return nil
 }
 
 // scManager launches two goroutines to receive PickResult and query whether the context of the stored PickResult is done.
-func (c *d7yBalancer) scManager() {
+func (b *d7yBalancer) scManager() {
 	// The first goroutine listens to the pickResultChan, put pickResults into a queue.
 	// Because the second go routine will reset a SubConn if there is no pickResult with the SubConn in the queue,
 	// and we want to hold a SubConn for a while to reuse it, I use a "shadow context" with timeout to achieve both.
 	go func() {
 		for {
-			pr := <-c.pickResultChan
-			c.pickResults.Add(pr)
+			pr := <-b.pickResultChan
+			b.pickResults.Add(pr)
 			// Use a shadow context to ensure one of the necessary conditions of calling resetSubConn is that at least a defined time duration has passed since each previous request.
 			// This trick can reduce a goroutine traversing the entire map and compare time.Now() and the recorded expired time.
 			shadowCtx, _ := context.WithTimeout(context.Background(), connectionLifetime)
-			c.pickResults.Add(pickResult{Ctx: shadowCtx, SC: pr.SC})
-			c.scCountsLock.Lock()
-			cnt, ok := c.scCounts[pr.SC]
+			b.pickResults.Add(pickResult{Ctx: shadowCtx, SC: pr.SC})
+			b.scCountsLock.Lock()
+			cnt, ok := b.scCounts[pr.SC]
 			if !ok {
 				cnt = new(int32)
 				*cnt = 0
-				c.scCounts[pr.SC] = cnt
+				b.scCounts[pr.SC] = cnt
 			}
 			// I want to use sync.map to replace the map scCounts, so I use atomic. But I find it (the replacement) is not easy...
 			atomic.AddInt32(cnt, 2)
-			c.scCountsLock.Unlock()
+			b.scCountsLock.Unlock()
 		}
 	}()
 
@@ -335,29 +328,29 @@ func (c *d7yBalancer) scManager() {
 	// It will reset a SubConn when there is no pickResults with the SubConn after dropped one.
 	go func() {
 		for {
-			v, shutdown := c.pickResults.Get()
+			v, shutdown := b.pickResults.Get()
 			if shutdown {
 				time.Sleep(connectionLifetime)
 				continue
 			}
 			pr := v.(pickResult)
-			c.pickResults.Done(v)
+			b.pickResults.Done(v)
 			select {
 			case <-pr.Ctx.Done():
-				c.scCountsLock.Lock()
-				cnt, ok := c.scCounts[pr.SC]
+				b.scCountsLock.Lock()
+				cnt, ok := b.scCounts[pr.SC]
 				if !ok {
-					c.scCountsLock.Unlock()
+					b.scCountsLock.Unlock()
 					break
 				}
 				atomic.AddInt32(cnt, -1)
 				if *cnt == 0 {
-					delete(c.scCounts, pr.SC)
-					c.resetSubConn(pr.SC)
+					delete(b.scCounts, pr.SC)
+					b.resetSubConn(pr.SC)
 				}
-				c.scCountsLock.Unlock()
+				b.scCountsLock.Unlock()
 			default:
-				c.pickResults.Add(pr)
+				b.pickResults.Add(pr)
 			}
 		}
 	}()
