9f7842da1bf1bbc1eb47aac0ba875223530be3f3
Gaius <gaius.qi@gmail.com>
2021-09-23 16:37:09 +0800

    feat: logger
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- client/daemon/storage/local_storage.go
+++ client/daemon/storage/local_storage.go
@@ -157,13 +157,13 @@ func (t *localTaskStore) ReadPiece(ctx context.Context, req *ReadPieceRequest) (
 		} else {
 			t.RUnlock()
 			file.Close()
-			logger.Errorf("invalid piece num: %d", req.Num)
+			t.Errorf("invalid piece num: %d", req.Num)
 			return nil, nil, ErrPieceNotFound
 		}
 	}
 	if _, err = file.Seek(req.Range.Start, io.SeekStart); err != nil {
 		file.Close()
-		logger.Errorf("file seek filed: %v", err)
+		t.Errorf("file seek filed: %v", err)
 		return nil, nil, err
 	}
 	// who call ReadPiece, who close the io.ReadCloser
@@ -178,7 +178,7 @@ func (t *localTaskStore) ReadAllPieces(ctx context.Context, req *PeerTaskMetaDat
 	}
 	if _, err = file.Seek(0, io.SeekStart); err != nil {
 		file.Close()
-		logger.Errorf("file seek filed: %v", err)
+		t.Errorf("file seek filed: %v", err)
 		return nil, err
 	}
 	// who call ReadPiece, who close the io.ReadCloser
