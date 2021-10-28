a23b3437fc31c60f92f88be8e26d4b18626314fa
zzy987 <67889264+zzy987@users.noreply.github.com>
2021-10-28 16:51:01 +0800

    feat: a test framework for balancer
    Signed-off-by: zzy987 <67889264+zzy987@users.noreply.github.com>

---
+++ pkg/rpc/balancer_test.go
@@ -0,0 +1,142 @@
+/*
+ *     Copyright 2020 The Dragonfly Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rpc
+
+//
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver/manual"
+	"google.golang.org/grpc/status"
+	testpb "google.golang.org/grpc/test/grpc_testing"
+)
+
+type tester struct{}
+
+func Test(t *testing.T) {
+	RunSubTests(t, tester{})
+}
+
+func RunSubTests(t *testing.T, x tester) {
+	xt := reflect.TypeOf(x)
+	xv := reflect.ValueOf(x)
+
+	for i := 0; i < xt.NumMethod(); i++ {
+		methodName := xt.Method(i).Name
+		if !strings.HasPrefix(methodName, "Test") {
+			continue
+		}
+		tfunc := xv.MethodByName(methodName).Interface().(func(*testing.T))
+		t.Run(strings.TrimPrefix(methodName, "Test"), func(t *testing.T) {
+			tfunc(t)
+		})
+	}
+}
+
+type testServer struct {
+	testpb.UnimplementedTestServiceServer
+}
+
+func newTestServer() *testServer {
+	return &testServer{}
+}
+
+func (s *testServer) EmptyCall(ctx context.Context, in *testpb.Empty) (*testpb.Empty, error) {
+	return &testpb.Empty{}, nil
+}
+
+type testServerData struct {
+	servers     []*grpc.Server
+	serverImpls []*testServer
+	addresses   []string
+}
+
+func (t *testServerData) cleanup() {
+	for _, s := range t.servers {
+		s.Stop()
+	}
+}
+
+func startTestServers(count int) (_ *testServerData, err error) {
+	t := &testServerData{}
+
+	defer func() {
+		if err != nil {
+			t.cleanup()
+		}
+	}()
+	for i := 0; i < count; i++ {
+		lis, err := net.Listen("tcp", "localhost:0")
+		if err != nil {
+			return nil, fmt.Errorf("failed to listen %v", err)
+		}
+
+		s := grpc.NewServer()
+		sImpl := newTestServer()
+		testpb.RegisterTestServiceServer(s, sImpl)
+		t.servers = append(t.servers, s)
+		t.serverImpls = append(t.serverImpls, sImpl)
+		t.addresses = append(t.addresses, lis.Addr().String())
+
+		go func(s *grpc.Server, l net.Listener) {
+			s.Serve(l)
+		}(s, lis)
+	}
+
+	return t, nil
+}
+
+func (tester) TestOneBackend(t *testing.T) {
+	r := manual.NewBuilderWithScheme("whatever")
+
+	test, err := startTestServers(1)
+	if err != nil {
+		t.Fatalf("failed to start servers: %v", err)
+	}
+	defer test.cleanup()
+
+	cc, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"loadBalancingPolicy": "%s"}`, d7yBalancerPolicy)))
+	if err != nil {
+		t.Fatalf("failed to dial: %v", err)
+	}
+	defer cc.Close()
+
+	testc := testpb.NewTestServiceClient(cc)
+	// The first RPC should fail because there's no address.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	defer cancel()
+	if _, err := testc.EmptyCall(ctx, &testpb.Empty{}); err == nil || status.Code(err) != codes.DeadlineExceeded {
+		t.Fatalf("EmptyCall() = _, %v, want _, DeadlineExceeded", err)
+	}
+
+	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: test.addresses[0]}}})
+	// The second RPC should succeed.
+	if _, err := testc.EmptyCall(context.Background(), &testpb.Empty{}); err != nil {
+		t.Fatalf("EmptyCall() = _, %v, want _, <nil>", err)
+	}
+}
