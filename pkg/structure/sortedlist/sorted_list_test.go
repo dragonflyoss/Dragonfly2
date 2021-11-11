ead43c258bb8a15b24d85c5ce0598354c70ca5f6
孙伟鹏 <weipeng.swp@alibaba-inc.com>
2021-11-11 23:07:57 +0800

    fix golangci
    Signed-off-by: 孙伟鹏 <weipeng.swp@alibaba-inc.com>

--- pkg/structure/sortedlist/sorted_list_test.go
+++ pkg/structure/sortedlist/sorted_list_test.go
@@ -23,9 +23,10 @@ import (
 	"sync"
 	"testing"
 
-	"d7y.io/dragonfly/v2/pkg/structure/sortedlist/mocks"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+
+	"d7y.io/dragonfly/v2/pkg/structure/sortedlist/mocks"
 )
 
 const N = 1000
