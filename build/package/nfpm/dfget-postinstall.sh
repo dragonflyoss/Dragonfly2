717b997e6ea8de2b48c4137fe07e2d72423271c7
Jim Ma <majinjing3@gmail.com>
2021-12-10 14:24:50 +0800

    feat: optmize dfget package upgrade support (#804)
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- build/package/nfpm/dfget-postinstall.sh
+++ build/package/nfpm/dfget-postinstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ "$D7Y_DEBUG" == "true" ]]; then
+  set -x
+  echo post install with param: "$@"
+fi
+
 chmod a+s /usr/bin/dfget
 
-which systemctl && (systemctl daemon-reload; systemctl enable dfget-daemon.service)
+if [[ -f /bin/systemctl || -f /usr/bin/systemctl ]]; then
+  systemctl daemon-reload; systemctl enable --now dfget-daemon.service
+fi
--- build/package/nfpm/dfget-preremove.sh
+++ build/package/nfpm/dfget-preremove.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-which systemctl && (systemctl stop dfget-daemon.service; systemctl disable dfget-daemon.service)
+if [[ "$D7Y_DEBUG" == "true" ]]; then
+  set -x
+  echo pre remove with param: "$@"
+fi
+
+upgrade=0
+
+# in rpm install:
+# refer: https://docs.fedoraproject.org/en-US/Fedora_Draft_Documentation/0.1/html/RPM_Guide/ch09s04s05.html
+if [[ "$1" -gt 0 ]]; then
+  upgrade=1
+fi
+
+# in deb install
+# refer: https://www.debian.org/doc/debian-policy/ch-maintainerscripts.html
+if [[ "$1" =~ "upgrade" ]]; then
+  upgrade=1
+fi
+
+# not in upgrade, purge systemd service
+if [[ "$upgrade" -eq 0 ]]; then
+  if [[ -f /bin/systemctl || -f /usr/bin/systemctl ]]; then
+    systemctl stop dfget-daemon.service; systemctl disable dfget-daemon.service
+  fi
+fi
