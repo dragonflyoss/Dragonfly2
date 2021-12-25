d4513f8c16a9600bee358d756b7ecd19d5f883a6
sunwp <244372610@qq.com>
2021-12-26 02:02:44 +0800

    grpc server interceptor
    Signed-off-by: sunwp <244372610@qq.com>

--- pkg/rpc/server.go
+++ pkg/rpc/server.go
@@ -17,22 +17,18 @@
 package rpc
 
 import (
-	"context"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	"time"
 
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
-	grpc_validator "github.com/grpc-ecosystem/go-grpc-middleware/validator"
-	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	"github.com/grpc-ecosystem/go-grpc-middleware/validator"
+	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/status"
 
-	"d7y.io/dragonfly/v2/internal/dferrors"
 	logger "d7y.io/dragonfly/v2/internal/dflog"
-	"d7y.io/dragonfly/v2/pkg/rpc/base"
-	"d7y.io/dragonfly/v2/pkg/rpc/base/common"
 )
 
 var DefaultServerOptions = []grpc.ServerOption{
@@ -45,48 +41,20 @@ var DefaultServerOptions = []grpc.ServerOption{
 		MaxConnectionIdle: 5 * time.Minute,
 	}),
 	grpc.MaxConcurrentStreams(100),
-	grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
-		streamServerInterceptor,
+	grpc.ChainStreamInterceptor(
 		grpc_prometheus.StreamServerInterceptor,
 		grpc_zap.StreamServerInterceptor(logger.GrpcLogger.Desugar()),
 		grpc_validator.StreamServerInterceptor(),
-	)),
-	grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-		unaryServerInterceptor,
+		grpc_recovery.StreamServerInterceptor(grpc_recovery.WithRecoveryHandler(func(p interface{}) (err error) {
+			return status.Errorf(codes.Unknown, "panic triggered: %v", p)
+		})),
+	),
+	grpc.ChainUnaryInterceptor(
 		grpc_prometheus.UnaryServerInterceptor,
 		grpc_zap.UnaryServerInterceptor(logger.GrpcLogger.Desugar()),
 		grpc_validator.UnaryServerInterceptor(),
-	)),
-}
-
-func streamServerInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-	err := handler(srv, ss)
-	if err != nil {
-		err = convertServerError(err)
-		logger.GrpcLogger.Errorf("do stream server error: %v for method: %s", err, info.FullMethod)
-	}
-	return err
-}
-
-func unaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	m, err := handler(ctx, req)
-	if err != nil {
-		err = convertServerError(err)
-		logger.GrpcLogger.Errorf("do unary server error: %v for method: %s", err, info.FullMethod)
-	}
-
-	return m, err
-}
-
-func convertServerError(err error) error {
-	if status.Code(err) == codes.InvalidArgument {
-		err = dferrors.New(base.Code_BadRequest, err.Error())
-	}
-	if v, ok := err.(*dferrors.DfError); ok {
-		logger.GrpcLogger.Errorf(v.Message)
-		if s, e := status.Convert(err).WithDetails(common.NewGrpcDfError(v.Code, v.Message)); e == nil {
-			err = s.Err()
-		}
-	}
-	return err
+		grpc_recovery.UnaryServerInterceptor(grpc_recovery.WithRecoveryHandler(func(p interface{}) (err error) {
+			return status.Errorf(codes.Unknown, "panic triggered: %v", p)
+		})),
+	),
 }
