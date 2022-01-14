dc3a8a2289e5b12dca7278943398f4b39329fa7d
sunwp <244372610@qq.com>
2022-01-14 16:54:33 +0800

    dfdaemon test
    Signed-off-by: sunwp <244372610@qq.com>

--- pkg/rpc/dfdaemon/client/client_test.go
+++ pkg/rpc/dfdaemon/client/client_test.go
@@ -31,7 +31,6 @@ import (
 	"d7y.io/dragonfly/v2/internal/dfnet"
 	"d7y.io/dragonfly/v2/pkg/idgen"
 	"d7y.io/dragonfly/v2/pkg/rpc/base"
-	"d7y.io/dragonfly/v2/pkg/rpc/cdnsystem"
 	"d7y.io/dragonfly/v2/pkg/rpc/dfdaemon"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
@@ -53,27 +52,27 @@ var _ dfdaemon.DaemonServer = (*testServer)(nil)
 type testServer struct {
 	dfdaemon.UnimplementedDaemonServer
 
-	progress  map[string][]*dfdaemon.DownResult
-	pieceInfo map[string][]*base.PieceInfo
-	addr      string
+	progress    map[string][]*dfdaemon.DownResult // url-> downloadResult
+	piecePacket map[string]*base.PiecePacket      // taskID->piecePacket
+	addr        string
 }
 
 func (s testServer) Download(request *dfdaemon.DownRequest, stream dfdaemon.Daemon_DownloadServer) error {
-	log.Printf("server %s receive obtain request %v", s.addr, request)
+	log.Printf("server %s receive Download request %v", s.addr, request)
 	md2, ok := metadata.FromIncomingContext(stream.Context())
 	if ok && md2.Get("excludeaddrs") != nil {
 		if sets.NewString(md2.Get("excludeaddrs")...).Has(s.addr) {
 			return status.Errorf(codes.NotFound, "task not found")
 		}
 	}
-	taskID := idgen.TaskID(request.Url, request.UrlMeta)
-	if strings.HasPrefix(taskID, "aaaaaaaaaaaaaaa") {
-		taskID = "aaaaaaaaaaaaaaa"
+	testURL := request.Url
+	if strings.HasPrefix(testURL, "https://www.dragonfly1.com") {
+		testURL = "https://www.dragonfly1.com"
 	}
-	if strings.HasPrefix(taskID, "bbbbbbbbbbbbbbb") {
-		taskID = "bbbbbbbbbbbbbbb"
+	if strings.HasPrefix(testURL, "https://www.dragonfly2.com") {
+		testURL = "https://www.dragonfly1.com"
 	}
-	pieceInfos, ok := s.progress[taskID]
+	pieceInfos, ok := s.progress[testURL]
 	if !ok {
 		return status.Errorf(codes.NotFound, "task not found")
 	}
@@ -93,13 +92,13 @@ func (s testServer) GetPieceTasks(ctx context.Context, request *base.PieceTaskRe
 	if strings.HasPrefix(request.TaskId, "bbbbbbbbbbbbbbb") {
 		request.TaskId = "bbbbbbbbbbbbbbb"
 	}
-	pieces, ok := s.pieceInfo[request.TaskId]
+	piecePacket, ok := s.piecePacket[request.TaskId]
 	if !ok {
 		return nil, status.Errorf(codes.NotFound, "task not found")
 	}
-	pieceInfos := make([]*base.PieceInfo, 0, len(pieces))
+	pieceInfos := make([]*base.PieceInfo, 0)
 	var count uint32 = 0
-	for _, info := range pieces {
+	for _, info := range piecePacket.PieceInfos {
 		if uint32(info.PieceNum) >= request.StartNum && (count < request.Limit || request.Limit <= 0) {
 			p := &base.PieceInfo{
 				PieceNum:    info.PieceNum,
@@ -114,34 +113,41 @@ func (s testServer) GetPieceTasks(ctx context.Context, request *base.PieceTaskRe
 		}
 	}
 	pp := &base.PiecePacket{
-		TaskId:     request.TaskId,
-		DstPid:     request.DstPid,
-		DstAddr:    fmt.Sprintf("%s:%s", "127.0.0.1", "8080"),
-		PieceInfos: pieceInfos,
-		//TotalPiece:    pieces[len(pieces)-1].TotalPieceCount,
-		//ContentLength: pieces[len(pieces)-1].ContentLength,
-		PieceMd5Sign: "",
+		TaskId:        request.TaskId,
+		DstPid:        request.DstPid,
+		DstAddr:       piecePacket.DstAddr,
+		PieceInfos:    pieceInfos,
+		TotalPiece:    piecePacket.TotalPiece,
+		ContentLength: piecePacket.ContentLength,
+		PieceMd5Sign:  piecePacket.PieceMd5Sign,
 	}
 	return pp, nil
 }
 
 func (s testServer) CheckHealth(ctx context.Context, empty *emptypb.Empty) (*emptypb.Empty, error) {
-	panic("implement me")
+	return &emptypb.Empty{}, nil
 }
 
-func loadTestData() (seedPieces map[string][]*base.PieceInfo, progress map[string][]*dfdaemon.DownResult) {
-	data, err := os.ReadFile("../testdata/download_progress.json")
+func loadTestData() (piecePacket map[string]*base.PiecePacket, progress map[string][]*dfdaemon.DownResult) {
+	progressData, err := os.ReadFile("../testdata/download_progress.json")
+	if err != nil {
+		log.Fatalf("failed to load progress info: %v", err)
+	}
+	if err := json.Unmarshal(progressData, &progress); err != nil {
+		log.Fatalf("failed to load progress info: %v", err)
+	}
+	piecesData, err := os.ReadFile("../testdata/pieces.json")
 	if err != nil {
 		log.Fatalf("failed to load seed piece info: %v", err)
 	}
-	if err := json.Unmarshal(data, &seedPieces); err != nil {
+	if err := json.Unmarshal(piecesData, &piecePacket); err != nil {
 		log.Fatalf("failed to load piece info: %v", err)
 	}
 	return
 }
 
-func newTestServer(addr string, pieces map[string][]*base.PieceInfo, progress map[string][]*dfdaemon.DownResult) *testServer {
-	s := &testServer{addr: addr, pieceInfo: pieces, progress: progress}
+func newTestServer(addr string, piecePacket map[string]*base.PiecePacket, progress map[string][]*dfdaemon.DownResult) *testServer {
+	s := &testServer{addr: addr, piecePacket: piecePacket, progress: progress}
 	return s
 }
 
@@ -197,15 +203,15 @@ func TestOneBackend(t *testing.T) {
 
 	client, err := GetClientByAddrs([]dfnet.NetAddr{{Addr: test.addresses[0]}})
 	if err != nil {
-		t.Fatalf("failed to get cdn client: %v", err)
+		t.Fatalf("failed to get daemon client: %v", err)
 	}
 	defer client.Close()
 
-	var testTask = "aaaaaaaaaaaaaaa"
-	verifyPieces := test.serverImpls[0].pieceInfo[testTask]
+	var testURL = "https://www.dragonfly1.com"
+	progressInfos := test.serverImpls[0].progress[testURL]
 	{
 		downReq := &dfdaemon.DownRequest{
-			Url:               "https://dragonfly.com",
+			Url:               testURL,
 			Output:            "",
 			Timeout:           0,
 			Limit:             0,
@@ -216,31 +222,32 @@ func TestOneBackend(t *testing.T) {
 		}
 		stream, err := client.Download(context.Background(), downReq)
 		if err != nil {
-			t.Fatalf("failed to call ObtainSeeds: %v", err)
+			t.Fatalf("failed to call Download: %v", err)
 		}
-		var pieceIndex int32
+		var index int32
 		for {
-			piece, err := stream.Recv()
+			progress, err := stream.Recv()
 			if err == io.EOF {
 				break
 			}
 			if err != nil {
-				t.Fatalf("failed to recv piece info: %v", err)
+				t.Fatalf("failed to recv progress info: %v", err)
 			}
-			if !cmp.Equal(piece, verifyPieces[pieceIndex], cmpopts.IgnoreUnexported(cdnsystem.PieceSeed{}), cmpopts.IgnoreUnexported(base.PieceInfo{})) {
-				t.Fatalf("failed to verify piece, want:%v, actual: %v", verifyPieces[pieceIndex], piece)
+			if !cmp.Equal(progress, progressInfos[index], cmpopts.IgnoreUnexported(dfdaemon.DownResult{})) {
+				t.Fatalf("failed to verify progress, want:%v, actual: %v", progressInfos[index], progress)
 			}
-			pieceIndex++
+			index++
 		}
 	}
 
 	{
+		testTaskID := "aaaaaaaaaaaaaaa"
 		req := &base.PieceTaskRequest{
-			TaskId:   testTask,
+			TaskId:   testTaskID,
 			SrcPid:   "peer1",
 			DstPid:   "peer2",
 			StartNum: 0,
-			Limit:    1,
+			Limit:    3,
 		}
 		piecePacket, err := client.GetPieceTasks(context.Background(), dfnet.NetAddr{Addr: test.addresses[0]}, req)
 
@@ -250,9 +257,10 @@ func TestOneBackend(t *testing.T) {
 		if len(piecePacket.PieceInfos) != int(req.Limit) {
 			t.Fatalf("piece count is not same with req, want: %d, actual: %d", req.Limit, len(piecePacket.PieceInfos))
 		}
+		verifyPieces := test.serverImpls[0].piecePacket[testTaskID].PieceInfos
 		var pieceIndex = 0
 		for {
-			if !cmp.Equal(piecePacket.PieceInfos[pieceIndex], verifyPieces[pieceIndex], cmpopts.IgnoreUnexported(cdnsystem.PieceSeed{}), cmpopts.IgnoreUnexported(base.PieceInfo{})) {
+			if !cmp.Equal(piecePacket.PieceInfos[pieceIndex], verifyPieces[pieceIndex], cmpopts.IgnoreUnexported(base.PieceInfo{})) {
 				t.Fatalf("failed to verify piece, want:%v, actual: %v", verifyPieces[pieceIndex], piecePacket.PieceInfos[pieceIndex])
 			}
 			pieceIndex++
@@ -264,7 +272,7 @@ func TestOneBackend(t *testing.T) {
 	{
 		err := client.CheckHealth(context.Background(), dfnet.NetAddr{Addr: test.addresses[0]})
 		if err != nil {
-
+			t.Fatalf("check health should ok, but got err: %v", err)
 		}
 	}
 }
@@ -282,12 +290,12 @@ func TestHashTask(t *testing.T) {
 		{Addr: test.addresses[2]},
 	})
 	if err != nil {
-		t.Fatalf("failed to get cdn client: %v", err)
+		t.Fatalf("failed to get dfdaemon client: %v", err)
 	}
 	defer client.Close()
 	serverHashRing := hashring.New(test.addresses)
 	{
-		var taskID = "aaaaaaaaaaaaaaa"
+		var taskID = idgen.TaskID("https://www.dragonfly1.com", nil)
 		selectedServer, ok := serverHashRing.GetNode(taskID)
 		if !ok {
 			t.Fatalf("failed to hash server node for task: %s", taskID)
@@ -300,11 +308,11 @@ func TestHashTask(t *testing.T) {
 			eg.Go(func() error {
 				var temp peer.Peer
 				stream, err := client.Download(context.Background(), &dfdaemon.DownRequest{
-					Url:     "https://dragonfly.com",
+					Url:     "https://www.dragonfly1.com",
 					UrlMeta: nil,
 				}, grpc.Peer(&temp))
 				if err != nil {
-					return errors.Errorf("failed to call ObtainSeeds: %v", err)
+					return errors.Errorf("failed to call Download: %v", err)
 				}
 				for {
 					_, err := stream.Recv()
@@ -312,7 +320,7 @@ func TestHashTask(t *testing.T) {
 						break
 					}
 					if err != nil {
-						return errors.Errorf("failed to recv ObtainSeeds: %v", err)
+						return errors.Errorf("failed to recv Download: %v", err)
 					}
 				}
 				serverPeerChan <- temp
@@ -331,12 +339,12 @@ func TestHashTask(t *testing.T) {
 			}
 		}()
 		if err := eg.Wait(); err != nil {
-			t.Fatalf("failed to call obtainSeeds: %v", err)
+			t.Fatalf("failed to call Download: %v", err)
 		}
 		close(serverPeerChan)
 		wg.Wait()
 		if serverPeerCount != totalCallCount {
-			t.Fatalf("call obtainSeeds success count is wrong, expect %d actual %d", totalCallCount, serverPeerCount)
+			t.Fatalf("call Download success count is wrong, expect %d actual %d", totalCallCount, serverPeerCount)
 		}
 		for _, addr := range serverAddrs {
 			if selectedServer != addr {
@@ -345,18 +353,18 @@ func TestHashTask(t *testing.T) {
 		}
 	}
 	{
-		var task1 = "aaaaaaaaaaaaaaa"
-		var task2 = "bbbbbbbbbbbbbbb"
-		selectedServer1, _ := serverHashRing.GetNode(task1)
-		selectedServer2, _ := serverHashRing.GetNode(task2)
+		var taskURL1 = "https://www.dragonfly1.com"
+		var taskURL2 = "https://www.dragonfly2.com"
+		selectedServer1, _ := serverHashRing.GetNode(idgen.TaskID(taskURL1, nil))
+		selectedServer2, _ := serverHashRing.GetNode(idgen.TaskID(taskURL2, nil))
 		for {
 			if selectedServer1 != selectedServer2 {
 				break
 			}
-			task1 = task1 + rand.String(3)
-			task2 = task2 + rand.String(3)
-			selectedServer1, _ = serverHashRing.GetNode(task1)
-			selectedServer2, _ = serverHashRing.GetNode(task2)
+			taskURL1 = taskURL1 + rand.String(3)
+			taskURL2 = taskURL2 + rand.String(3)
+			selectedServer1, _ = serverHashRing.GetNode(idgen.TaskID(taskURL1, nil))
+			selectedServer2, _ = serverHashRing.GetNode(idgen.TaskID(taskURL2, nil))
 		}
 		// test hash different taskID to different server node
 		var serverPeer1 peer.Peer
@@ -364,11 +372,11 @@ func TestHashTask(t *testing.T) {
 		eg := errgroup.Group{}
 		eg.Go(func() error {
 			stream, err := client.Download(context.Background(), &dfdaemon.DownRequest{
-				Url:     "https://dragonfly.com",
+				Url:     taskURL1,
 				UrlMeta: nil,
 			}, grpc.Peer(&serverPeer1))
 			if err != nil {
-				return errors.Errorf("failed to call ObtainSeeds: %v", err)
+				return errors.Errorf("failed to call Download: %v", err)
 			}
 			for {
 				_, err := stream.Recv()
@@ -376,18 +384,18 @@ func TestHashTask(t *testing.T) {
 					break
 				}
 				if err != nil {
-					return errors.Errorf("failed to recv ObtainSeeds: %v", err)
+					return errors.Errorf("failed to recv Download: %v", err)
 				}
 			}
 			return nil
 		})
 		eg.Go(func() error {
 			stream, err := client.Download(context.Background(), &dfdaemon.DownRequest{
-				Url:     "https://dragonfly2.com",
+				Url:     taskURL2,
 				UrlMeta: nil,
 			}, grpc.Peer(&serverPeer2))
 			if err != nil {
-				return errors.Errorf("failed to call ObtainSeeds: %v", err)
+				return errors.Errorf("failed to call Download: %v", err)
 			}
 			for {
 				_, err := stream.Recv()
@@ -395,13 +403,13 @@ func TestHashTask(t *testing.T) {
 					break
 				}
 				if err != nil {
-					return errors.Errorf("failed to recv ObtainSeeds: %v", err)
+					return errors.Errorf("failed to recv Download: %v", err)
 				}
 			}
 			return nil
 		})
 		if err := eg.Wait(); err != nil {
-			t.Fatalf("failed to call obtainSeeds: %v", err)
+			t.Fatalf("failed to call Download: %v", err)
 		}
 		if serverPeer1.Addr.String() != selectedServer1 {
 			t.Fatalf("failed to hash to expected server, want %s, actual %s", selectedServer1, serverPeer1.Addr.String())
@@ -432,23 +440,23 @@ func TestMigration(t *testing.T) {
 	defer client.Close()
 	{
 		// exclude len(serverHashRing)-1 server nodes, only left one
-		var taskID = "aaaaaaaaaaaaaaa"
-		verifyPieces := test.serverImpls[0].pieceInfo[taskID]
+		var testTaskURL = "https://www.dragonfly1.com"
+		testTaskID := idgen.TaskID(testTaskURL, nil)
 		serverHashRing := hashring.New(test.addresses)
-		candidateAddrs, _ := serverHashRing.GetNodes(taskID, len(test.servers))
-		var serverPeer peer.Peer
+		candidateAddrs, _ := serverHashRing.GetNodes(testTaskID, len(test.servers))
 		var excludeAddrs []string
 		for _, addr := range candidateAddrs[0 : len(candidateAddrs)-1] {
 			excludeAddrs = append(excludeAddrs, "excludeAddrs", addr)
 		}
+		var serverPeer peer.Peer
 		ctx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs(excludeAddrs...))
 		stream, err := client.Download(ctx, &dfdaemon.DownRequest{
-			Url:     "https://dragonfly.com",
+			Url:     testTaskURL,
 			UrlMeta: nil,
 		}, grpc.Peer(&serverPeer))
 
 		if err != nil {
-			t.Fatalf("failed to call ObtainSeeds: %v", err)
+			t.Fatalf("failed to call Download: %v", err)
 		}
 		for {
 			_, err := stream.Recv()
@@ -456,42 +464,20 @@ func TestMigration(t *testing.T) {
 				break
 			}
 			if err != nil {
-				t.Fatalf("failed to recieve ObtainSeeds: %v", err)
+				t.Fatalf("failed to recieve Download: %v", err)
 			}
 		}
 		if serverPeer.Addr.String() != candidateAddrs[len(candidateAddrs)-1] {
 			t.Fatalf("target server addr is not same as expected, want: %s, actual: %s", candidateAddrs[len(candidateAddrs)-1], serverPeer.Addr.String())
 		}
-		req := &base.PieceTaskRequest{
-			TaskId:   taskID,
-			SrcPid:   "peer1",
-			DstPid:   "peer2",
-			StartNum: 0,
-			Limit:    5,
-		}
-		piecePacket, err := client.GetPieceTasks(context.Background(), dfnet.NetAddr{Addr: serverPeer.Addr.String()}, req)
-		if err != nil {
-			t.Fatalf("failed to call GetPieceTasks: %v", err)
-		}
-		if len(piecePacket.PieceInfos) != int(req.Limit) {
-			t.Fatalf("piece count is not same with req, want: %d, actual: %d", req.Limit, len(piecePacket.PieceInfos))
-		}
-		var pieceIndex = 0
-		for {
-			if !cmp.Equal(piecePacket.PieceInfos[pieceIndex], verifyPieces[pieceIndex], cmpopts.IgnoreUnexported(cdnsystem.PieceSeed{}), cmpopts.IgnoreUnexported(base.PieceInfo{})) {
-				t.Fatalf("failed to verify piece, want:%v, actual: %v", verifyPieces[pieceIndex], piecePacket.PieceInfos[pieceIndex])
-			}
-			pieceIndex++
-			if pieceIndex == int(req.Limit) {
-				break
-			}
-		}
 	}
+
 	{
 		// all server node failed
-		var taskID = "aaaaaaaaaaaaaaa"
+		var testTaskURL = "https://www.dragonfly1.com"
+		testTaskID := idgen.TaskID(testTaskURL, nil)
 		serverHashRing := hashring.New(test.addresses)
-		candidateAddrs, _ := serverHashRing.GetNodes(taskID, len(test.servers))
+		candidateAddrs, _ := serverHashRing.GetNodes(testTaskID, len(test.servers))
 		var serverPeer peer.Peer
 		var excludeAddrs []string
 		for _, addr := range candidateAddrs {
@@ -499,16 +485,16 @@ func TestMigration(t *testing.T) {
 		}
 		ctx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs(excludeAddrs...))
 		stream, err := client.Download(ctx, &dfdaemon.DownRequest{
-			Url:     "https://dragonfly.com",
+			Url:     "https://dragonfly1.com",
 			UrlMeta: nil,
-		})
+		}, grpc.Peer(&serverPeer))
 
 		if err != nil {
-			t.Fatalf("failed to call ObtainSeeds: %v", err)
+			t.Fatalf("failed to call Download: %v", err)
 		}
 		_, err = stream.Recv()
 		if err == nil || !cmp.Equal(err.Error(), status.Errorf(codes.NotFound, "task not found").Error()) {
-			t.Fatalf("obtain seeds want err task not found, but got %v", err)
+			t.Fatalf("download want err task not found, but got %v", err)
 		}
 		if serverPeer.Addr.String() != candidateAddrs[len(candidateAddrs)-1] {
 			t.Fatalf("target server addr is not same as expected, want: %s, actual: %s", candidateAddrs[len(candidateAddrs)-1], serverPeer.Addr.String())
