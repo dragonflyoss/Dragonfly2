24dc35186b3e9828d52649600041bc140c318180
Jim Ma <majinjing3@gmail.com>
2022-01-22 22:03:39 +0800

    chore: update gen-buildx.sh
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- hack/gen-buildx.sh
+++ hack/gen-buildx.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 components="cdn scheduler manager"
-components="cdn"
 
 set -x
 
@@ -10,3 +9,8 @@ for c in ${components}; do
   sed -i '1i# syntax=docker/dockerfile:1.3' "${file}"
   sed -i "s#RUN make build-$c && make install-$c#RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/root/go/ export GOPATH=/root/go \&\& make build-$c \&\& make install-$c#" "${file}"
 done
+
+sed -i '1i# syntax=docker/dockerfile:1.3' build/images/dfdaemon/Dockerfile
+sed -i "s#RUN make build-dfget && make install-dfget#RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/root/go/ export GOPATH=/root/go \&\& make build-dfget \&\& make install-dfget#" build/images/dfdaemon/Dockerfile
+
+sed -i 's/docker build/docker build --load/' hack/docker-build.sh
