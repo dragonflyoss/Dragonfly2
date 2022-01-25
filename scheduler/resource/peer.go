56aafae7c3bbd32d1dde0d76825b0cefe00d49b5
Gaius <gaius.qi@gmail.com>
2022-01-25 14:20:41 +0800

    feat: cdn download tiny file
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- scheduler/resource/peer.go
+++ scheduler/resource/peer.go
@@ -390,7 +390,7 @@ func (p *Peer) DeleteStream() {
 
 // Download tiny file from peer
 func (p *Peer) DownloadTinyFile(ctx context.Context) ([]byte, error) {
-	// Download url: http://${host}:${port}/download/${taskIndex}/${taskID}?peerId=scheduler;
+	// Download url: http://${host}:${port}/download/${taskIndex}/${taskID}?peerId=scheduler
 	url := url.URL{
 		Scheme:   "http",
 		Host:     fmt.Sprintf("%s:%d", p.Host.IP, p.Host.DownloadPort),
@@ -411,7 +411,10 @@ func (p *Peer) DownloadTinyFile(ctx context.Context) ([]byte, error) {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
+	// The HTTP 206 Partial Content success status response code indicates that
+	// the request has succeeded and the body contains the requested ranges of data, as described in the Range header of the request.
+	// Refer to https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/206
+	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusBadRequest {
 		return []byte{}, fmt.Errorf("%v: %v", url.String(), resp.Status)
 	}
 
