4ca1d38a80bbd6fe328745d872c4bb2ea5d3395f
Jim Ma <majinjing3@gmail.com>
2022-03-18 11:11:40 +0800

    fix: subscriber data race
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- client/daemon/rpcserver/subscriber.go
+++ client/daemon/rpcserver/subscriber.go
@@ -153,11 +153,11 @@ loop:
 				s.Unlock()
 				break loop
 			}
-			s.Unlock()
 			if info.Finished {
 				break loop
 			}
 			nextPieceNum = s.searchNextPieceNum(nextPieceNum)
+			s.Unlock()
 		case <-s.Success:
 			s.Lock()
 			// all pieces already sent
