05088908b6729b5ff2b2ecb166bfa4b0647d2c27
Gaius <gaius.qi@gmail.com>
2022-04-20 18:13:02 +0800

    feat: move dfnet to pkg dir (#1265)
    Signed-off-by: Gaius <gaius.qi@gmail.com>
    Signed-off-by: 李龙峰 <lilongfeng@inspur.com>

--- pkg/rpc/manager/client/client.go
+++ pkg/rpc/manager/client/client.go
@@ -29,7 +29,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 
 	logger "d7y.io/dragonfly/v2/internal/dflog"
-	"d7y.io/dragonfly/v2/internal/dfnet"
+	"d7y.io/dragonfly/v2/pkg/dfnet"
 	"d7y.io/dragonfly/v2/pkg/reachable"
 	"d7y.io/dragonfly/v2/pkg/rpc/manager"
 )
