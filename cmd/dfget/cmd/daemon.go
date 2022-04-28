1917b6a2c4a8efbfa866752aef553dcb70871701
Jim Ma <majinjing3@gmail.com>
2022-04-28 18:28:44 +0800

    feat: support build arm&&arm64 dfget (#1286)
    Signed-off-by: Jim Ma <majinjing3@gmail.com>
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- cmd/dfget/cmd/daemon.go
+++ cmd/dfget/cmd/daemon.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"os"
 	"path"
-	"syscall"
 	"time"
 
 	"github.com/gofrs/flock"
@@ -90,14 +89,14 @@ func redirectStdoutAndStderr(console bool, logDir string) {
 		stdoutPath := path.Join(logDir, "daemon", "stdout.log")
 		if stdout, err := os.OpenFile(stdoutPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND|os.O_SYNC, 0644); err != nil {
 			logger.Warnf("open %s error: %s", stdoutPath, err)
-		} else if err := syscall.Dup2(int(stdout.Fd()), 1); err != nil {
+		} else if err := dup2(int(stdout.Fd()), 1); err != nil {
 			logger.Warnf("redirect stdout error: %s", err)
 		}
 	}
 	stderrPath := path.Join(logDir, "daemon", "stderr.log")
 	if stderr, err := os.OpenFile(stderrPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND|os.O_SYNC, 0644); err != nil {
 		logger.Warnf("open %s error: %s", stderrPath, err)
-	} else if err := syscall.Dup2(int(stderr.Fd()), 2); err != nil {
+	} else if err := dup2(int(stderr.Fd()), 2); err != nil {
 		logger.Warnf("redirect stderr error: %s", err)
 	}
 }
