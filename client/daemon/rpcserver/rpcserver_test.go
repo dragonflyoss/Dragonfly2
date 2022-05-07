a57e37679c57564366285b292a70798d15b94d81
Jim Ma <majinjing3@gmail.com>
2022-05-07 15:21:25 +0800

    fix: unit test
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- client/daemon/rpcserver/rpcserver_test.go
+++ client/daemon/rpcserver/rpcserver_test.go
@@ -32,6 +32,7 @@ import (
 
 	"d7y.io/dragonfly/v2/client/clientutil"
 	"d7y.io/dragonfly/v2/client/daemon/peer"
+	"d7y.io/dragonfly/v2/client/daemon/storage"
 	mock_peer "d7y.io/dragonfly/v2/client/daemon/test/mock/peer"
 	mock_storage "d7y.io/dragonfly/v2/client/daemon/test/mock/storage"
 	"d7y.io/dragonfly/v2/pkg/dfnet"
@@ -419,6 +420,15 @@ func Test_SyncPieceTasks(t *testing.T) {
 							TotalPiece:    int32(tc.totalPieces),
 							ContentLength: int64(tc.totalPieces) * int64(pieceSize),
 							PieceMd5Sign:  "",
+						}, nil
+					})
+				mockStorageManger.EXPECT().GetExtendAttribute(gomock.Any(),
+					gomock.Any()).AnyTimes().DoAndReturn(
+					func(ctx context.Context, req *storage.PeerTaskMetadata) (*base.ExtendAttribute, error) {
+						return &base.ExtendAttribute{
+							Header: map[string]string{
+								"Test": "test",
+							},
 						}, nil
 					})
 				mockTaskManager := mock_peer.NewMockTaskManager(ctrl)
