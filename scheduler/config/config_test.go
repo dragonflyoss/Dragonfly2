0be2d9e1bf9d348af6e9d679199d54bc7cde0bec
ljx373327 <ljx373327@alibaba-inc.com>
2022-07-05 16:00:47 +0800

    Added unit tests on configuration
    Signed-off-by: ljx373327 <ljx373327@alibaba-inc.com>

--- scheduler/config/config_test.go
+++ scheduler/config/config_test.go
@@ -34,14 +34,6 @@ func TestConfig_Load(t *testing.T) {
 	assert := testifyassert.New(t)
 
 	config := &Config{
-		Server: &ServerConfig{
-			IP:       "127.0.0.1",
-			Host:     "foo",
-			Listen:   "0.0.0.0",
-			Port:     8002,
-			CacheDir: "foo",
-			LogDir:   "bar",
-		},
 		Scheduler: &SchedulerConfig{
 			Algorithm:            "default",
 			BackSourceCount:      3,
@@ -57,13 +49,16 @@ func TestConfig_Load(t *testing.T) {
 				HostTTL:        10 * time.Minute,
 			},
 		},
+		Server: &ServerConfig{
+			IP:       "127.0.0.1",
+			Host:     "foo",
+			Listen:   "0.0.0.0",
+			Port:     8002,
+			CacheDir: "foo",
+			LogDir:   "bar",
+		},
 		DynConfig: &DynConfig{
 			RefreshInterval: 5 * time.Minute,
-		},
-		Host: &HostConfig{
-			IDC:         "foo",
-			NetTopology: "bar",
-			Location:    "baz",
 		},
 		Manager: &ManagerConfig{
 			Addr:               "127.0.0.1:65003",
@@ -74,6 +69,11 @@ func TestConfig_Load(t *testing.T) {
 		},
 		SeedPeer: &SeedPeerConfig{
 			Enable: true,
+		},
+		Host: &HostConfig{
+			IDC:         "foo",
+			NetTopology: "bar",
+			Location:    "baz",
 		},
 		Job: &JobConfig{
 			Enable:             true,
