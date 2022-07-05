2185959c90834890c706f640b1e609cbc3e99b3c
Gaius <gaius.qi@gmail.com>
2022-07-05 20:12:26 +0800

    feat: change local cache ttl (#1436)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- manager/config/config.go
+++ manager/config/config.go
@@ -245,7 +245,7 @@ func New() *Config {
 			},
 			Local: &LocalCacheConfig{
 				Size: 10000,
-				TTL:  30 * time.Second,
+				TTL:  10 * time.Second,
 			},
 		},
 		ObjectStorage: &ObjectStorageConfig{
