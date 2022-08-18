8fdb6c1af5eaff6ccdf4e7fbf531a92b0aad3a45
Gaius <gaius.qi@gmail.com>
2022-08-18 20:15:34 +0800

    feat: dynconfig refresh and notify listeners (#1579)
    Signed-off-by: Gaius <gaius.qi@gmail.com>
    Signed-off-by: 李龙峰 <lilongfeng@inspur.com>

--- scheduler/config/dynconfig.go
+++ scheduler/config/dynconfig.go
@@ -259,7 +259,15 @@ func (d *dynconfig) Get() (*DynconfigData, error) {
 
 // Refresh refreshes dynconfig in cache.
 func (d *dynconfig) Refresh() error {
-	return d.Dynconfig.Refresh()
+	if err := d.Dynconfig.Refresh(); err != nil {
+		return err
+	}
+
+	if err := d.Notify(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Register allows an instance to register itself to listen/observe events.
