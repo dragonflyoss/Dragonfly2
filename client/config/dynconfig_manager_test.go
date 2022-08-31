7cde54fd77fe0d6926e96a5f286773af1866684e
Gaius <gaius.qi@gmail.com>
2022-08-31 11:41:32 +0800

    feat: resolver addr add ServerName (#1614)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- client/config/dynconfig_manager_test.go
+++ client/config/dynconfig_manager_test.go
@@ -93,7 +93,7 @@ func TestDynconfigGetResolveSchedulerAddrs_ManagerSourceType(t *testing.T) {
 				assert := assert.New(t)
 				result, err := dynconfig.GetResolveSchedulerAddrs()
 				assert.NoError(err)
-				assert.EqualValues(result, []resolver.Address{{Addr: "127.0.0.1:3000"}})
+				assert.EqualValues(result, []resolver.Address{{ServerName: "127.0.0.1", Addr: "127.0.0.1:3000"}})
 			},
 		},
 		{
@@ -139,7 +139,7 @@ func TestDynconfigGetResolveSchedulerAddrs_ManagerSourceType(t *testing.T) {
 				assert := assert.New(t)
 				result, err := dynconfig.GetResolveSchedulerAddrs()
 				assert.NoError(err)
-				assert.EqualValues(result, []resolver.Address{{Addr: "127.0.0.1:3000"}})
+				assert.EqualValues(result, []resolver.Address{{ServerName: "127.0.0.1", Addr: "127.0.0.1:3000"}})
 			},
 		},
 		{
@@ -235,7 +235,7 @@ func TestDynconfigGetResolveSchedulerAddrs_ManagerSourceType(t *testing.T) {
 				assert := assert.New(t)
 				result, err := dynconfig.GetResolveSchedulerAddrs()
 				assert.NoError(err)
-				assert.EqualValues(result, []resolver.Address{{Addr: "127.0.0.1:3000"}})
+				assert.EqualValues(result, []resolver.Address{{ServerName: "127.0.0.1", Addr: "127.0.0.1:3000"}})
 			},
 		},
 		{
@@ -280,7 +280,7 @@ func TestDynconfigGetResolveSchedulerAddrs_ManagerSourceType(t *testing.T) {
 				assert := assert.New(t)
 				result, err := dynconfig.GetResolveSchedulerAddrs()
 				assert.NoError(err)
-				assert.EqualValues(result, []resolver.Address{{Addr: "127.0.0.1:3000"}})
+				assert.EqualValues(result, []resolver.Address{{ServerName: "127.0.0.1", Addr: "127.0.0.1:3000"}})
 			},
 		},
 		{
--- scheduler/config/dynconfig.go
+++ scheduler/config/dynconfig.go
@@ -35,7 +35,6 @@ import (
 	"d7y.io/dragonfly/v2/manager/types"
 	"d7y.io/dragonfly/v2/pkg/reachable"
 	managerclient "d7y.io/dragonfly/v2/pkg/rpc/manager/client"
-	"d7y.io/dragonfly/v2/pkg/slices"
 )
 
 var (
@@ -177,23 +176,26 @@ func (d *dynconfig) GetResolveSeedPeerAddrs() ([]resolver.Address, error) {
 		return nil, err
 	}
 
-	addrs := []string{}
+	var (
+		addrs        = map[string]bool{}
+		resolveAddrs = []resolver.Address{}
+	)
 	for _, seedPeer := range seedPeers {
 		addr := fmt.Sprintf("%s:%d", seedPeer.IP, seedPeer.Port)
 		r := reachable.New(&reachable.Config{Address: addr})
 		if err := r.Check(); err != nil {
 			logger.Warnf("seed peer address %s is unreachable", addr)
 		} else {
-			addrs = append(addrs, addr)
-			continue
-		}
-	}
+			if addrs[addr] {
+				continue
+			}
 
-	resolveAddrs := []resolver.Address{}
-	for _, addr := range slices.RemoveDuplicates(addrs) {
-		resolveAddrs = append(resolveAddrs, resolver.Address{
-			Addr: addr,
-		})
+			resolveAddrs = append(resolveAddrs, resolver.Address{
+				ServerName: seedPeer.IP,
+				Addr:       addr,
+			})
+			addrs[addr] = true
+		}
 	}
 
 	return resolveAddrs, nil
