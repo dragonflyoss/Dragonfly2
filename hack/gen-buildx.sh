8eee587fa09a4f8e1144f3a890cf684efee1f088
Jim Ma <majinjing3@gmail.com>
2022-09-02 12:28:26 +0800

    feat: update gen-buildx.sh
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- hack/gen-buildx.sh
+++ hack/gen-buildx.sh
@@ -7,14 +7,14 @@ set -x
 for c in ${components}; do
   file=build/images/"${c}"/Dockerfile
   sed -i '1i# syntax=docker/dockerfile:1.3' "${file}"
-  sed -i "s#RUN make build-$c && make install-$c#RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/go/pkg export GOPATH=/root/go \&\& make build-$c \&\& make install-$c#" "${file}"
+  sed -i "s#RUN make build-$c && make install-$c#RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/go/pkg make build-$c \&\& make install-$c#" "${file}"
   echo $c Dockerfile:
   cat $file
 done
 
 # dfdaemon is subcommand, need to update alone
 sed -i '1i# syntax=docker/dockerfile:1.3' build/images/dfdaemon/Dockerfile
-sed -i "s#RUN make build-dfget && make install-dfget#RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/go/pkg export GOPATH=/root/go \&\& make build-dfget \&\& make install-dfget#" build/images/dfdaemon/Dockerfile
+sed -i "s#RUN make build-dfget && make install-dfget#RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/go/pkg make build-dfget \&\& make install-dfget#" build/images/dfdaemon/Dockerfile
 echo dfdaemon Dockerfile:
 cat build/images/dfdaemon/Dockerfile
 
