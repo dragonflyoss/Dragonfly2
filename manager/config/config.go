cfc12b3962bfbd2dbe4789ea69e9833a63487e81
glin-gogogo <100889576+glin-gogogo@users.noreply.github.com>
2022-10-14 16:43:45 +0800

    feat: obs object storage support (#1758)
    Signed-off-by: 李龙峰 <lilongfeng@inspur.com>

--- manager/config/config.go
+++ manager/config/config.go
@@ -486,7 +486,7 @@ func (cfg *Config) Validate() error {
 			return errors.New("objectStorage requires parameter name")
 		}
 
-		if !slices.Contains([]string{objectstorage.ServiceNameS3, objectstorage.ServiceNameOSS}, cfg.ObjectStorage.Name) {
+		if !slices.Contains([]string{objectstorage.ServiceNameS3, objectstorage.ServiceNameOSS, objectstorage.ServiceNameOBS}, cfg.ObjectStorage.Name) {
 			return errors.New("objectStorage requires parameter name")
 		}
 
