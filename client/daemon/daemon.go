e6faa889733d57f2a5ee3bc034cd1d049cd34532
cuidajun <cuidajun.cdj@alibaba-inc.com>
2022-10-27 10:23:24 +0800

    feat: daemon call leaveHost when exit (#1788)
    Signed-off-by: bigerous <cuidajun.cdj@alibaba-inc.com>
    
    Signed-off-by: bigerous <cuidajun.cdj@alibaba-inc.com>
    Signed-off-by: 李龙峰 <lilongfeng@inspur.com>

--- client/daemon/daemon.go
+++ client/daemon/daemon.go
@@ -765,6 +765,19 @@ func (cd *clientDaemon) Serve() error {
 func (cd *clientDaemon) Stop() {
 	cd.once.Do(func() {
 		close(cd.done)
+		if cd.schedulerClient != nil {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+			if err := cd.schedulerClient.LeaveHost(ctx, &schedulerv1.LeaveHostRequest{Id: cd.schedPeerHost.Id}); err != nil {
+				logger.Errorf("daemon leaveHost error: %s", err.Error())
+			}
+			if err := cd.schedulerClient.Close(); err != nil {
+				logger.Errorf("scheduler client failed to stop: %s", err.Error())
+			} else {
+				logger.Info("scheduler client closed")
+			}
+		}
+
 		cd.GCManager.Stop()
 		cd.RPCManager.Stop()
 		if err := cd.UploadManager.Stop(); err != nil {
@@ -803,14 +816,6 @@ func (cd *clientDaemon) Stop() {
 				logger.Errorf("manager client failed to stop: %s", err.Error())
 			} else {
 				logger.Info("manager client closed")
-			}
-		}
-
-		if cd.schedulerClient != nil {
-			if err := cd.schedulerClient.Close(); err != nil {
-				logger.Errorf("scheduler client failed to stop: %s", err.Error())
-			} else {
-				logger.Info("scheduler client closed")
 			}
 		}
 	})
