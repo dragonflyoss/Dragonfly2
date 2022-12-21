c91fa68be7e79220fa823761b94d329bfb0a9160
Jim Ma <majinjing3@gmail.com>
2022-12-21 17:52:33 +0800

    chore: optimize download log
    Signed-off-by: Jim Ma <majinjing3@gmail.com>

--- pkg/source/list_metadata.go
+++ pkg/source/list_metadata.go
@@ -146,18 +146,19 @@ func (lm *ListMetadataClient) Download(request *Request) (*Response, error) {
 		}
 	}
 
-	log.Infof("list dirs cost: %dms, download cost: %dms", listMilliseconds, time.Now().Sub(start).Milliseconds())
-
 	metadata := ListMetadata{
 		URLEntries: allURLEntries,
 	}
 	data, _ := json.Marshal(metadata)
+
+	log.Infof("list dirs cost: %dms, entries: %d, metadata size: %d, download cost: %dms",
+		listMilliseconds, len(allURLEntries), len(data), time.Now().Sub(start).Milliseconds())
 	return &Response{
 		Status:     "OK",
 		StatusCode: http.StatusOK,
-		Header: Header(map[string][]string{
+		Header: map[string][]string{
 			headers.Expires: {request.Header.Get(ListMetadataExpire)},
-		}),
+		},
 		Body:          io.NopCloser(bytes.NewBuffer(data)),
 		ContentLength: int64(len(data)),
 		Validate: func() error {
