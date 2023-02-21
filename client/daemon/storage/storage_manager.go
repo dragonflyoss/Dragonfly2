dragonflyoss/Dragonfly2

commit 86a6030c8f1056e5a67e98e126d7ccc4c6b9ef75

feat: unregister failed task storage (#1717)
Signed-off-by: Jim Ma <majinjing3@gmail.com>

client/daemon/storage/storage_manager.go
@@ -20,6 +20,7 @@ package storage
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -80,6 +81,9 @@ type Reclaimer interface {
 
 	// MarkReclaim marks the storage which will be reclaimed
 	MarkReclaim()
+
+	// marks the special storage invalid, that will reclaim next gc round
+	MarkInvalid()
 
 	// Reclaim reclaims the storage
 	Reclaim() error
@@ -709,7 +713,7 @@ func (s *storageManager) ReloadPersistentTask(gcCallback GCCallback) error {
 				loadErrs = append(loadErrs, err0)
 				loadErrDirs = append(loadErrDirs, dataDir)
 				logger.With("action", "reload", "stage", "parse metadata", "taskID", taskID, "peerID", peerID).
-					Warnf("load task from disk error: %s", err0)
+					Warnf("load task from disk error: %s, data base64 encode: %s", err0, base64.StdEncoding.EncodeToString(bytes))
 				continue
 			}
 			logger.Debugf("load task %s/%s from disk, metadata %s, last access: %v, expire time: %s",
@@ -896,7 +900,7 @@ func (s *storageManager) deleteTask(meta PeerTaskMetadata) error {
 	} else {
 		s.cleanSubIndex(meta.TaskID, meta.PeerID)
 	}
-	task.(Reclaimer).MarkReclaim()
+	task.(Reclaimer).MarkInvalid()
 	return task.(Reclaimer).Reclaim()
 }
 

client/daemon/peer/peertask_conductor.go
@@ -1640,6 +1640,13 @@ func (pt *peerTaskConductor) fail() {
 		if pt.pieceTaskSyncManager != nil {
 			pt.pieceTaskSyncManager.cancel()
 		}
+		// mark storage to reclaim
+		_ = pt.StorageManager.UnregisterTask(
+			pt.ctx,
+			storage.CommonTaskRequest{
+				PeerID: pt.peerID,
+				TaskID: pt.taskID,
+			})
 	}()
 	pt.peerTaskManager.PeerTaskDone(pt.taskID)
 	var end = time.Now()

client/daemon/storage/local_storage.go
@@ -486,6 +486,10 @@ func (t *localTaskStore) CanReclaim() bool {
 	return reclaim
 }
 
+func (t *localTaskStore) MarkInvalid() {
+	t.invalid.Store(true)
+}
+
 // MarkReclaim will try to invoke gcCallback (normal leave peer task)
 func (t *localTaskStore) MarkReclaim() {
 	if t.reclaimMarked.Load() {

client/daemon/storage/local_storage_subtask.go
@@ -391,6 +391,13 @@ func (t *localSubTaskStore) CanReclaim() bool {
 	return false
 }
 
+func (t *localSubTaskStore) MarkInvalid() {
+	if t.parent.Done || t.invalid.Load() {
+		return
+	}
+	t.parent.MarkInvalid()
+}
+
 func (t *localSubTaskStore) MarkReclaim() {
 	t.parent.gcCallback(CommonTaskRequest{
 		PeerID: t.PeerID,

client/daemon/storage/mocks/stroage_manager_mock.go
@@ -224,6 +224,18 @@ func (mr *MockReclaimerMockRecorder) CanReclaim() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CanReclaim", reflect.TypeOf((*MockReclaimer)(nil).CanReclaim))
 }
 
+// MarkInvalid mocks base method.
+func (m *MockReclaimer) MarkInvalid() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "MarkInvalid")
+}
+
+// MarkInvalid indicates an expected call of MarkInvalid.
+func (mr *MockReclaimerMockRecorder) MarkInvalid() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "MarkInvalid", reflect.TypeOf((*MockReclaimer)(nil).MarkInvalid))
+}
+
 // MarkReclaim mocks base method.
 func (m *MockReclaimer) MarkReclaim() {
 	m.ctrl.T.Helper()

commit bccee5c1e9478a0b5d64c486a1d2523b76cb6555

fix: unregister task from scheduler in storage.deleteTask
On graceful shutdown we will call forceGC() to delete all cached tasks
if 'keepStorage' is false. And we rely on deleteTask() to delete the
given task from both local storage and scheduler.

But commit 86a6030c8f10 ("feat: unregister failed task storage (#1717)")
changed MarkReclaim() to MarkInvalid() in deleteTask(), and resulted in
task leak in scheduler.

Fix it by calling MarkReclaim() in deleteTask().

Fixes: 86a6030c8f10 ("feat: unregister failed task storage (#1717)")
Signed-off-by: Eryu Guan <eguan@linux.alibaba.com>

client/daemon/storage/storage_manager.go
@@ -886,6 +886,7 @@ func (s *storageManager) TryGC() (bool, error) {
 	return true, nil
 }
 
+// delete the given task from local storage and unregister it from scheduler.
 func (s *storageManager) deleteTask(meta PeerTaskMetadata) error {
 	task, ok := s.LoadAndDeleteTask(meta)
 	if !ok {
@@ -899,7 +900,8 @@ func (s *storageManager) deleteTask(meta PeerTaskMetadata) error {
 	} else {
 		s.cleanSubIndex(meta.TaskID, meta.PeerID)
 	}
-	task.(Reclaimer).MarkInvalid()
+	// MarkReclaim() will call gcCallback, which will unregister task from scheduler
+	task.(Reclaimer).MarkReclaim()
 	return task.(Reclaimer).Reclaim()
 }
 