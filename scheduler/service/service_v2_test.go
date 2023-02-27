a346b6fb1959e80d55b0d7bab6f4b3dd677a2e50
Gaius <gaius.qi@gmail.com>
2023-02-27 12:37:44 +0800

    feat: correct grpc error code and implement StatPeer and LeavePeer
    Correct grpc error from codes.Unknow to codes.Internal in gorm
    operation. Implement StatPeer and LeavePeer interface in v2 version of
    grpc.
    
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- scheduler/service/service_v2_test.go
+++ scheduler/service/service_v2_test.go
@@ -17,11 +17,19 @@
 package service
 
 import (
+	"context"
 	"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	commonv2 "d7y.io/api/pkg/apis/common/v2"
+	schedulerv2 "d7y.io/api/pkg/apis/scheduler/v2"
 
 	"d7y.io/dragonfly/v2/scheduler/config"
 	configmocks "d7y.io/dragonfly/v2/scheduler/config/mocks"
@@ -53,6 +61,251 @@ func TestService_NewV2(t *testing.T) {
 			dynconfig := configmocks.NewMockDynconfigInterface(ctl)
 			storage := storagemocks.NewMockStorage(ctl)
 			tc.expect(t, NewV2(&config.Config{Scheduler: mockSchedulerConfig}, resource, scheduling, dynconfig, storage))
+		})
+	}
+}
+
+func TestServiceV2_StatPeer(t *testing.T) {
+	tests := []struct {
+		name   string
+		mock   func(peer *resource.Peer, peerManager resource.PeerManager, ms *mocks.MockSchedulingMockRecorder, mr *resource.MockResourceMockRecorder, mp *resource.MockPeerManagerMockRecorder)
+		expect func(t *testing.T, peer *resource.Peer, resp *commonv2.Peer, err error)
+	}{
+		{
+			name: "peer not found",
+			mock: func(peer *resource.Peer, peerManager resource.PeerManager, ms *mocks.MockSchedulingMockRecorder, mr *resource.MockResourceMockRecorder, mp *resource.MockPeerManagerMockRecorder) {
+				gomock.InOrder(
+					mr.PeerManager().Return(peerManager).Times(1),
+					mp.Load(gomock.Any()).Return(nil, false).Times(1),
+				)
+			},
+			expect: func(t *testing.T, peer *resource.Peer, resp *commonv2.Peer, err error) {
+				assert := assert.New(t)
+				assert.ErrorIs(err, status.Errorf(codes.NotFound, "peer %s not found", mockPeerID))
+			},
+		},
+		{
+			name: "peer has been loaded",
+			mock: func(peer *resource.Peer, peerManager resource.PeerManager, ms *mocks.MockSchedulingMockRecorder, mr *resource.MockResourceMockRecorder, mp *resource.MockPeerManagerMockRecorder) {
+				peer.StorePiece(&mockPiece)
+				peer.Task.StorePiece(&mockPiece)
+				gomock.InOrder(
+					mr.PeerManager().Return(peerManager).Times(1),
+					mp.Load(gomock.Any()).Return(peer, true).Times(1),
+				)
+			},
+			expect: func(t *testing.T, peer *resource.Peer, resp *commonv2.Peer, err error) {
+				assert := assert.New(t)
+				assert.EqualValues(resp, &commonv2.Peer{
+					Id: peer.ID,
+					Range: &commonv2.Range{
+						Start:  peer.Range.Start,
+						Length: peer.Range.Length,
+					},
+					Priority: peer.Priority,
+					Pieces: []*commonv2.Piece{
+						{
+							Number:      mockPiece.Number,
+							ParentId:    mockPiece.ParentID,
+							Offset:      mockPiece.Offset,
+							Length:      mockPiece.Length,
+							Digest:      mockPiece.Digest.String(),
+							TrafficType: mockPiece.TrafficType,
+							Cost:        durationpb.New(mockPiece.Cost),
+							CreatedAt:   timestamppb.New(mockPiece.CreatedAt),
+						},
+					},
+					Cost:  durationpb.New(peer.Cost.Load()),
+					State: peer.FSM.Current(),
+					Task: &commonv2.Task{
+						Id:            peer.Task.ID,
+						Type:          peer.Task.Type,
+						Url:           peer.Task.URL,
+						Digest:        peer.Task.Digest.String(),
+						Tag:           peer.Task.Tag,
+						Application:   peer.Task.Application,
+						Filters:       peer.Task.Filters,
+						Header:        peer.Task.Header,
+						PieceLength:   peer.Task.PieceLength,
+						ContentLength: peer.Task.ContentLength.Load(),
+						PieceCount:    peer.Task.TotalPieceCount.Load(),
+						SizeScope:     peer.Task.SizeScope(),
+						Pieces: []*commonv2.Piece{
+							{
+								Number:      mockPiece.Number,
+								ParentId:    mockPiece.ParentID,
+								Offset:      mockPiece.Offset,
+								Length:      mockPiece.Length,
+								Digest:      mockPiece.Digest.String(),
+								TrafficType: mockPiece.TrafficType,
+								Cost:        durationpb.New(mockPiece.Cost),
+								CreatedAt:   timestamppb.New(mockPiece.CreatedAt),
+							},
+						},
+						State:     peer.Task.FSM.Current(),
+						PeerCount: int32(peer.Task.PeerCount()),
+						CreatedAt: timestamppb.New(peer.Task.CreatedAt.Load()),
+						UpdatedAt: timestamppb.New(peer.Task.UpdatedAt.Load()),
+					},
+					Host: &commonv2.Host{
+						Id:              peer.Host.ID,
+						Type:            uint32(peer.Host.Type),
+						Hostname:        peer.Host.Hostname,
+						Ip:              peer.Host.IP,
+						Port:            peer.Host.Port,
+						DownloadPort:    peer.Host.DownloadPort,
+						Os:              peer.Host.OS,
+						Platform:        peer.Host.Platform,
+						PlatformFamily:  peer.Host.PlatformFamily,
+						PlatformVersion: peer.Host.PlatformVersion,
+						KernelVersion:   peer.Host.KernelVersion,
+						Cpu: &commonv2.CPU{
+							LogicalCount:   peer.Host.CPU.LogicalCount,
+							PhysicalCount:  peer.Host.CPU.PhysicalCount,
+							Percent:        peer.Host.CPU.Percent,
+							ProcessPercent: peer.Host.CPU.ProcessPercent,
+							Times: &commonv2.CPUTimes{
+								User:      peer.Host.CPU.Times.User,
+								System:    peer.Host.CPU.Times.System,
+								Idle:      peer.Host.CPU.Times.Idle,
+								Nice:      peer.Host.CPU.Times.Nice,
+								Iowait:    peer.Host.CPU.Times.Iowait,
+								Irq:       peer.Host.CPU.Times.Irq,
+								Softirq:   peer.Host.CPU.Times.Softirq,
+								Steal:     peer.Host.CPU.Times.Steal,
+								Guest:     peer.Host.CPU.Times.Guest,
+								GuestNice: peer.Host.CPU.Times.GuestNice,
+							},
+						},
+						Memory: &commonv2.Memory{
+							Total:              peer.Host.Memory.Total,
+							Available:          peer.Host.Memory.Available,
+							Used:               peer.Host.Memory.Used,
+							UsedPercent:        peer.Host.Memory.UsedPercent,
+							ProcessUsedPercent: peer.Host.Memory.ProcessUsedPercent,
+							Free:               peer.Host.Memory.Free,
+						},
+						Network: &commonv2.Network{
+							TcpConnectionCount:       peer.Host.Network.TCPConnectionCount,
+							UploadTcpConnectionCount: peer.Host.Network.UploadTCPConnectionCount,
+							SecurityDomain:           peer.Host.Network.SecurityDomain,
+							Location:                 peer.Host.Network.Location,
+							Idc:                      peer.Host.Network.IDC,
+						},
+						Disk: &commonv2.Disk{
+							Total:             peer.Host.Disk.Total,
+							Free:              peer.Host.Disk.Free,
+							Used:              peer.Host.Disk.Used,
+							UsedPercent:       peer.Host.Disk.UsedPercent,
+							InodesTotal:       peer.Host.Disk.InodesTotal,
+							InodesUsed:        peer.Host.Disk.InodesUsed,
+							InodesFree:        peer.Host.Disk.InodesFree,
+							InodesUsedPercent: peer.Host.Disk.InodesUsedPercent,
+						},
+						Build: &commonv2.Build{
+							GitVersion: peer.Host.Build.GitVersion,
+							GitCommit:  peer.Host.Build.GitCommit,
+							GoVersion:  peer.Host.Build.GoVersion,
+							Platform:   peer.Host.Build.Platform,
+						},
+					},
+					NeedBackToSource: peer.NeedBackToSource.Load(),
+					CreatedAt:        timestamppb.New(peer.CreatedAt.Load()),
+					UpdatedAt:        timestamppb.New(peer.UpdatedAt.Load()),
+				})
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			scheduling := mocks.NewMockScheduling(ctl)
+			res := resource.NewMockResource(ctl)
+			dynconfig := configmocks.NewMockDynconfigInterface(ctl)
+			storage := storagemocks.NewMockStorage(ctl)
+			peerManager := resource.NewMockPeerManager(ctl)
+			mockHost := resource.NewHost(
+				mockRawHost.ID, mockRawHost.IP, mockRawHost.Hostname,
+				mockRawHost.Port, mockRawHost.DownloadPort, mockRawHost.Type)
+			mockTask := resource.NewTask(mockTaskID, mockTaskURL, mockTaskTag, mockTaskApplication, commonv2.TaskType_DFDAEMON, mockTaskFilters, mockTaskHeader, mockTaskBackToSourceLimit, resource.WithDigest(mockTaskDigest), resource.WithPieceLength(mockTaskPieceLength))
+			peer := resource.NewPeer(mockSeedPeerID, mockTask, mockHost, resource.WithRange(mockPeerRange))
+			svc := NewV2(&config.Config{Scheduler: mockSchedulerConfig, Metrics: config.MetricsConfig{EnableHost: true}}, res, scheduling, dynconfig, storage)
+
+			tc.mock(peer, peerManager, scheduling.EXPECT(), res.EXPECT(), peerManager.EXPECT())
+			resp, err := svc.StatPeer(context.Background(), &schedulerv2.StatPeerRequest{TaskId: mockTaskID, PeerId: mockPeerID})
+			tc.expect(t, peer, resp, err)
+		})
+	}
+}
+
+func TestService_LeavePeer(t *testing.T) {
+	tests := []struct {
+		name   string
+		mock   func(peer *resource.Peer, peerManager resource.PeerManager, ms *mocks.MockSchedulingMockRecorder, mr *resource.MockResourceMockRecorder, mp *resource.MockPeerManagerMockRecorder)
+		expect func(t *testing.T, err error)
+	}{
+		{
+			name: "peer not found",
+			mock: func(peer *resource.Peer, peerManager resource.PeerManager, ms *mocks.MockSchedulingMockRecorder, mr *resource.MockResourceMockRecorder, mp *resource.MockPeerManagerMockRecorder) {
+				gomock.InOrder(
+					mr.PeerManager().Return(peerManager).Times(1),
+					mp.Load(gomock.Any()).Return(nil, false).Times(1),
+				)
+			},
+			expect: func(t *testing.T, err error) {
+				assert := assert.New(t)
+				assert.ErrorIs(err, status.Errorf(codes.NotFound, "peer %s not found", mockPeerID))
+			},
+		},
+		{
+			name: "peer fsm event failed",
+			mock: func(peer *resource.Peer, peerManager resource.PeerManager, ms *mocks.MockSchedulingMockRecorder, mr *resource.MockResourceMockRecorder, mp *resource.MockPeerManagerMockRecorder) {
+				peer.FSM.SetState(resource.PeerStateLeave)
+				gomock.InOrder(
+					mr.PeerManager().Return(peerManager).Times(1),
+					mp.Load(gomock.Any()).Return(peer, true).Times(1),
+				)
+			},
+			expect: func(t *testing.T, err error) {
+				assert := assert.New(t)
+				assert.ErrorIs(err, status.Error(codes.FailedPrecondition, "peer fsm event failed: event Leave inappropriate in current state Leave"))
+			},
+		},
+		{
+			name: "peer leaves succeeded",
+			mock: func(peer *resource.Peer, peerManager resource.PeerManager, ms *mocks.MockSchedulingMockRecorder, mr *resource.MockResourceMockRecorder, mp *resource.MockPeerManagerMockRecorder) {
+				gomock.InOrder(
+					mr.PeerManager().Return(peerManager).Times(1),
+					mp.Load(gomock.Any()).Return(peer, true).Times(1),
+				)
+			},
+			expect: func(t *testing.T, err error) {
+				assert := assert.New(t)
+				assert.NoError(err)
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			scheduling := mocks.NewMockScheduling(ctl)
+			res := resource.NewMockResource(ctl)
+			dynconfig := configmocks.NewMockDynconfigInterface(ctl)
+			storage := storagemocks.NewMockStorage(ctl)
+			peerManager := resource.NewMockPeerManager(ctl)
+			mockHost := resource.NewHost(
+				mockRawHost.ID, mockRawHost.IP, mockRawHost.Hostname,
+				mockRawHost.Port, mockRawHost.DownloadPort, mockRawHost.Type)
+			mockTask := resource.NewTask(mockTaskID, mockTaskURL, mockTaskTag, mockTaskApplication, commonv2.TaskType_DFDAEMON, mockTaskFilters, mockTaskHeader, mockTaskBackToSourceLimit, resource.WithDigest(mockTaskDigest), resource.WithPieceLength(mockTaskPieceLength))
+			peer := resource.NewPeer(mockSeedPeerID, mockTask, mockHost, resource.WithRange(mockPeerRange))
+			svc := NewV2(&config.Config{Scheduler: mockSchedulerConfig, Metrics: config.MetricsConfig{EnableHost: true}}, res, scheduling, dynconfig, storage)
+
+			tc.mock(peer, peerManager, scheduling.EXPECT(), res.EXPECT(), peerManager.EXPECT())
+			tc.expect(t, svc.LeavePeer(context.Background(), &schedulerv2.LeavePeerRequest{TaskId: mockTaskID, PeerId: mockPeerID}))
 		})
 	}
 }
