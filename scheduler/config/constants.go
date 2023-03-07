b64d025f49a27e667cdcbb44861b16aeebc377e3
wenmanxiang <wenmanxiang@baidu.com>
2023-03-07 19:53:09 +0800

    feat: add advertise port
    Change-Id: I56bd34bc6a45e07aafc452d3fe9c74c7126c4494

--- scheduler/config/constants.go
+++ scheduler/config/constants.go
@@ -43,6 +43,7 @@ const (
 const (
 	// DefaultServerPort is default port for server.
 	DefaultServerPort = 8002
+
 	// DefaultServerAdvertisePort is default advertise port for server.
 	DefaultServerAdvertisePort = 8004
 )
