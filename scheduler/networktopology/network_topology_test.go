004bbc317f16122176f166b2bbd3c64aedb67eef
XZ <834756128@qq.com>
2023-03-16 14:58:49 +0800

    feat: add network topology package
    Signed-off-by: XZ <834756128@qq.com>

--- scheduler/networktopology/network_topology_test.go
+++ scheduler/networktopology/network_topology_test.go
@@ -242,3 +242,139 @@ func TestNewNetworkTopology_StoreParents(t *testing.T) {
 		})
 	}
 }
+
+func TestNetworkTopology_DeleteParents(t *testing.T) {
+	tests := []struct {
+		name   string
+		config *config.Config
+		expect func(t *testing.T, networkTopology NetworkTopology, host *resource.Host, m *sync.Map)
+	}{
+		{
+			name:   "delete parents",
+			config: config.New(),
+			expect: func(t *testing.T, networkTopology NetworkTopology, host *resource.Host, m *sync.Map) {
+				assert := assert.New(t)
+				networkTopology.StoreParents(host.ID, m)
+				networkTopology.DeleteParents(host.ID)
+				_, loaded := networkTopology.LoadParents(host.ID)
+				assert.Equal(loaded, false)
+			},
+		},
+		{
+			name:   "delete key does not exist",
+			config: config.New(),
+			expect: func(t *testing.T, networkTopology NetworkTopology, host *resource.Host, m *sync.Map) {
+				assert := assert.New(t)
+				host.ID = ""
+				networkTopology.StoreParents(host.ID, m)
+				networkTopology.DeleteParents(host.ID)
+				_, loaded := networkTopology.LoadParents(host.ID)
+				assert.Equal(loaded, false)
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			probes := NewProbes(mockSrcHost)
+			probes.StoreProbe(mockProbe)
+			mockManagerClient := mocks.NewMockV2(ctl)
+			res := resource.NewMockResource(ctl)
+			n, err := NewNetworkTopology(tc.config, res, mockManagerClient, WithTransportCredentials(nil))
+			if err != nil {
+				t.Fatal(err)
+			}
+			var m sync.Map
+			m.Store(mockProbe.Host.ID, mockProbe)
+			tc.expect(t, n, mockSrcHost, &m)
+		})
+	}
+}
+
+func TestNetworkTopology_LoadEdge(t *testing.T) {
+	tests := []struct {
+		name   string
+		config *config.Config
+		expect func(t *testing.T, networkTopology NetworkTopology, src *resource.Host, dest *resource.Host, probes *Probes)
+	}{
+		{
+			name:   "load edge",
+			config: config.New(),
+			expect: func(t *testing.T, networkTopology NetworkTopology, src *resource.Host, dest *resource.Host, probes *Probes) {
+				assert := assert.New(t)
+				networkTopology.StoreParents(src.ID, &sync.Map{})
+				networkTopology.StoreEdge(src.ID, dest.ID, probes)
+				p, loaded := networkTopology.LoadEdge(src.ID, dest.ID)
+				assert.Equal(loaded, true)
+				assert.Equal(p.Host.ID, probes.Host.ID)
+			},
+		},
+		{
+			name:   "parents does not exist",
+			config: config.New(),
+			expect: func(t *testing.T, networkTopology NetworkTopology, src *resource.Host, dest *resource.Host, probes *Probes) {
+				assert := assert.New(t)
+				_, parentsLoaded := networkTopology.LoadParents(src.ID)
+				assert.Equal(parentsLoaded, false)
+				_, edgeLoaded := networkTopology.LoadEdge(src.ID, dest.ID)
+				assert.Equal(edgeLoaded, false)
+			},
+		},
+		{
+			name:   "parents exists but edge does not exist",
+			config: config.New(),
+			expect: func(t *testing.T, networkTopology NetworkTopology, src *resource.Host, dest *resource.Host, probes *Probes) {
+				assert := assert.New(t)
+				networkTopology.StoreParents(src.ID, &sync.Map{})
+				_, parentsLoaded := networkTopology.LoadParents(src.ID)
+				assert.Equal(parentsLoaded, true)
+				_, edgeLoaded := networkTopology.LoadEdge(src.ID, dest.ID)
+				assert.Equal(edgeLoaded, false)
+			},
+		},
+		{
+			name:   "load source key is empty",
+			config: config.New(),
+			expect: func(t *testing.T, networkTopology NetworkTopology, src *resource.Host, dest *resource.Host, probes *Probes) {
+				assert := assert.New(t)
+				src.ID = ""
+				networkTopology.StoreParents(src.ID, &sync.Map{})
+				networkTopology.StoreEdge(src.ID, dest.ID, probes)
+				p, loaded := networkTopology.LoadEdge(src.ID, dest.ID)
+				assert.Equal(loaded, true)
+				assert.Equal(p.Host.ID, probes.Host.ID)
+			},
+		},
+		{
+			name:   "load destination key is empty",
+			config: config.New(),
+			expect: func(t *testing.T, networkTopology NetworkTopology, src *resource.Host, dest *resource.Host, probes *Probes) {
+				assert := assert.New(t)
+				dest.ID = ""
+				networkTopology.StoreParents(src.ID, &sync.Map{})
+				networkTopology.StoreEdge(src.ID, dest.ID, probes)
+				p, loaded := networkTopology.LoadEdge(src.ID, dest.ID)
+				assert.Equal(loaded, true)
+				assert.Equal(p.Host.ID, probes.Host.ID)
+
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			probes := NewProbes(mockSrcHost)
+			probes.StoreProbe(mockProbe)
+			mockManagerClient := mocks.NewMockV2(ctl)
+			res := resource.NewMockResource(ctl)
+			n, err := NewNetworkTopology(tc.config, res, mockManagerClient, WithTransportCredentials(nil))
+			if err != nil {
+				t.Fatal(err)
+			}
+			tc.expect(t, n, mockSrcHost, mockDestHost, probes)
+		})
+	}
+}
