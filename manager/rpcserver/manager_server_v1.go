d981fd0c478f8c20127584dbbabb0869b97d363a
Gaius <gaius.qi@gmail.com>
2023-03-31 19:46:58 +0800

    feat: change error log to warn log when cache miss
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- manager/rpcserver/manager_server_v1.go
+++ manager/rpcserver/manager_server_v1.go
@@ -97,7 +97,7 @@ func (s *managerServerV1) GetSeedPeer(ctx context.Context, req *managerv1.GetSee
 	// Cache hit.
 	var pbSeedPeer managerv1.SeedPeer
 	if err := s.cache.Get(ctx, cacheKey, &pbSeedPeer); err != nil {
-		log.Errorf("%s cache miss because of %s", cacheKey, err.Error())
+		log.Warnf("%s cache miss because of %s", cacheKey, err.Error())
 	} else {
 		log.Debugf("%s cache hit", cacheKey)
 		return &pbSeedPeer, nil
@@ -262,7 +262,7 @@ func (s *managerServerV1) GetScheduler(ctx context.Context, req *managerv1.GetSc
 	// Cache hit.
 	var pbScheduler managerv1.Scheduler
 	if err := s.cache.Get(ctx, cacheKey, &pbScheduler); err != nil {
-		log.Errorf("%s cache miss because of %s", cacheKey, err.Error())
+		log.Warnf("%s cache miss because of %s", cacheKey, err.Error())
 	} else {
 		log.Debugf("%s cache hit", cacheKey)
 		return &pbScheduler, nil
@@ -457,7 +457,7 @@ func (s *managerServerV1) ListSchedulers(ctx context.Context, req *managerv1.Lis
 	cacheKey := cache.MakeSchedulersCacheKeyForPeer(req.Hostname, req.Ip)
 
 	if err := s.cache.Get(ctx, cacheKey, &pbListSchedulersResponse); err != nil {
-		log.Errorf("%s cache miss because of %s", cacheKey, err.Error())
+		log.Warnf("%s cache miss because of %s", cacheKey, err.Error())
 	} else {
 		log.Debugf("%s cache hit", cacheKey)
 		return &pbListSchedulersResponse, nil
@@ -576,7 +576,7 @@ func (s *managerServerV1) ListBuckets(ctx context.Context, req *managerv1.ListBu
 
 	// Cache hit.
 	if err := s.cache.Get(ctx, cacheKey, &pbListBucketsResponse); err != nil {
-		log.Errorf("%s cache miss because of %s", cacheKey, err.Error())
+		log.Warnf("%s cache miss because of %s", cacheKey, err.Error())
 	} else {
 		log.Debugf("%s cache hit", cacheKey)
 		return &pbListBucketsResponse, nil
@@ -616,7 +616,7 @@ func (s *managerServerV1) ListApplications(ctx context.Context, req *managerv1.L
 	var pbListApplicationsResponse managerv1.ListApplicationsResponse
 	cacheKey := cache.MakeApplicationsCacheKey()
 	if err := s.cache.Get(ctx, cacheKey, &pbListApplicationsResponse); err != nil {
-		log.Errorf("%s cache miss because of %s", cacheKey, err.Error())
+		log.Warnf("%s cache miss because of %s", cacheKey, err.Error())
 	} else {
 		log.Debugf("%s cache hit", cacheKey)
 		return &pbListApplicationsResponse, nil
