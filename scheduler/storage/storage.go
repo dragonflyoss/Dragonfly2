eb13dea547be09850b4713fb3b272798af65ec38
XZ <834756128@qq.com>
2023-04-06 13:14:27 +0800

    refactor: implements the generic interface
    Signed-off-by: XZ <834756128@qq.com>

--- scheduler/storage/storage.go
+++ scheduler/storage/storage.go
@@ -43,6 +43,12 @@ const (
 
 	// RecordFileExt is extension of record file name.
 	RecordFileExt = "csv"
+
+	// ProbesFilePrefix is prefix of probes file name.
+	ProbesFilePrefix = "probes"
+
+	// ProbesFileExt is extension of probes file name.
+	ProbesFileExt = "csv"
 )
 
 const (
@@ -55,95 +61,145 @@ const (
 
 // Storage is the interface used for storage.
 type Storage interface {
-	// Create inserts the record into csv file.
-	Create(Record) error
+	// Create inserts the data into csv file.
+	Create(data any) error
 
-	// List returns all of records in csv file.
-	List() ([]Record, error)
+	// List returns specified data in csv file.
+	List(data any) (any, error)
 
-	// Count returns the count of records.
-	Count() int64
+	// RecordCount returns the count of records.
+	RecordCount() int64
 
-	// Open opens storage for read, it returns io.ReadCloser of storage files.
-	Open() (io.ReadCloser, error)
+	// ProbesCount returns the count of probes.
+	ProbesCount() int64
 
-	// Clear removes all record files.
-	Clear() error
+	// Open opens storage for read, it returns io.ReadCloser of specified storage files.
+	Open(data any) (io.ReadCloser, error)
+
+	// Clear removes specified data.
+	Clear(data any) error
 }
 
 // storage provides storage function.
 type storage struct {
 	baseDir    string
-	filename   string
 	maxSize    int64
 	maxBackups int
-	buffer     []Record
 	bufferSize int
-	count      int64
 	mu         *sync.RWMutex
+
+	recordFilename string
+	recordBuffer   []Record
+	recordCount    int64
+
+	probesFilename string
+	probesBuffer   []Probes
+	probesCount    int64
 }
 
-// New returns a new Storage instence.
+// New returns a new Storage instance.
 func New(baseDir string, maxSize, maxBackups, bufferSize int) (Storage, error) {
 	s := &storage{
 		baseDir:    baseDir,
-		filename:   filepath.Join(baseDir, fmt.Sprintf("%s.%s", RecordFilePrefix, RecordFileExt)),
 		maxSize:    int64(maxSize * megabyte),
 		maxBackups: maxBackups,
-		buffer:     make([]Record, 0, bufferSize),
 		bufferSize: bufferSize,
 		mu:         &sync.RWMutex{},
+
+		recordFilename: filepath.Join(baseDir, fmt.Sprintf("%s.%s", RecordFilePrefix, RecordFileExt)),
+		recordBuffer:   make([]Record, 0, bufferSize),
+		probesFilename: filepath.Join(baseDir, fmt.Sprintf("%s.%s", ProbesFilePrefix, ProbesFileExt)),
+		probesBuffer:   make([]Probes, 0, bufferSize),
 	}
 
-	file, err := os.OpenFile(s.filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	RecordFile, err := os.OpenFile(s.recordFilename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		return nil, err
 	}
-	file.Close()
+	RecordFile.Close()
+
+	ProbesFile, err := os.OpenFile(s.probesFilename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return nil, err
+	}
+	ProbesFile.Close()
 
 	return s, nil
 }
 
-// Create inserts the record into csv file.
-func (s *storage) Create(record Record) error {
+// Create inserts the data into csv file.
+func (s *storage) Create(data any) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	// Write without buffer.
-	if s.bufferSize == 0 {
-		if err := s.create(s.buffer...); err != nil {
-			return err
+
+	switch data.(type) {
+	case Record:
+		if s.bufferSize == 0 {
+			if err := s.create(s.recordBuffer); err != nil {
+				return err
+			}
+
+			// Update record count.
+			s.recordCount++
+			return nil
 		}
 
-		// Update record count.
-		s.count++
+		// Write records to file.
+		if len(s.recordBuffer) >= s.bufferSize {
+			if err := s.create(s.recordBuffer); err != nil {
+				return err
+			}
+
+			// Update record count.
+			s.recordCount += int64(s.bufferSize)
+
+			// Keep allocated memory.
+			s.recordBuffer = s.recordBuffer[:0]
+		}
+
+		// Write records to buffer.
+		s.recordBuffer = append(s.recordBuffer, data.(Record))
 		return nil
-	}
+	case Probes:
+		if s.bufferSize == 0 {
+			if err := s.create(s.probesBuffer); err != nil {
+				return err
+			}
 
-	// Write records to file.
-	if len(s.buffer) >= s.bufferSize {
-		if err := s.create(s.buffer...); err != nil {
-			return err
+			// Update probes count.
+			s.probesCount++
+			return nil
 		}
 
-		// Update record count.
-		s.count += int64(s.bufferSize)
+		// Write probes to file.
+		if len(s.probesBuffer) >= s.bufferSize {
+			if err := s.create(s.probesBuffer); err != nil {
+				return err
+			}
 
-		// Keep allocated memory.
-		s.buffer = s.buffer[:0]
+			// Update probes count.
+			s.probesCount += int64(s.bufferSize)
+
+			// Keep allocated memory.
+			s.probesBuffer = s.probesBuffer[:0]
+		}
+
+		// Write probes to buffer.
+		s.probesBuffer = append(s.probesBuffer, data.(Probes))
+		return nil
+	default:
+		return errors.New("invalid storage type")
 	}
-
-	// Write records to buffer.
-	s.buffer = append(s.buffer, record)
-	return nil
 }
 
-// List returns all of records in csv file.
-func (s *storage) List() ([]Record, error) {
+// List returns specified data in csv file.
+func (s *storage) List(data any) (any, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	fileInfos, err := s.backups()
+	fileInfos, err := s.backups(data)
 	if err != nil {
 		return nil, err
 	}
@@ -168,25 +224,42 @@ func (s *storage) List() ([]Record, error) {
 		readClosers = append(readClosers, file)
 	}
 
-	var records []Record
-	if err := gocsv.UnmarshalWithoutHeaders(io.MultiReader(readers...), &records); err != nil {
-		return nil, err
+	switch data.(type) {
+	case Record:
+		var records []Record
+		if err := gocsv.UnmarshalWithoutHeaders(io.MultiReader(readers...), &records); err != nil {
+			return nil, err
+		}
+
+		return records, nil
+	case Probes:
+		var probes []Probes
+		if err := gocsv.UnmarshalWithoutHeaders(io.MultiReader(readers...), &probes); err != nil {
+			return nil, err
+		}
+
+		return probes, nil
+	default:
+		return nil, errors.New("invalid data type")
 	}
-
-	return records, nil
 }
 
-// Count returns the count of records.
-func (s *storage) Count() int64 {
-	return s.count
+// RecordCount returns the count of records.
+func (s *storage) RecordCount() int64 {
+	return s.recordCount
 }
 
-// Open opens storage for read, it returns io.ReadCloser of storage files.
-func (s *storage) Open() (io.ReadCloser, error) {
+// ProbesCount returns the count of probes.
+func (s *storage) ProbesCount() int64 {
+	return s.probesCount
+}
+
+// Open opens storage for read, it returns io.ReadCloser of specified storage files.
+func (s *storage) Open(data any) (io.ReadCloser, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	fileInfos, err := s.backups()
+	fileInfos, err := s.backups(data)
 	if err != nil {
 		return nil, err
 	}
@@ -204,12 +277,12 @@ func (s *storage) Open() (io.ReadCloser, error) {
 	return pkgio.MultiReadCloser(readClosers...), nil
 }
 
-// Clear removes all records.
-func (s *storage) Clear() error {
+// Clear removes specified data.
+func (s *storage) Clear(data any) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	fileInfos, err := s.backups()
+	fileInfos, err := s.backups(data)
 	if err != nil {
 		return err
 	}
@@ -224,35 +297,43 @@ func (s *storage) Clear() error {
 	return nil
 }
 
-// create inserts the records into csv file.
-func (s *storage) create(records ...Record) error {
-	file, err := s.openFile()
+// create inserts the data into csv file.
+func (s *storage) create(data any) error {
+	file, err := s.openFile(data)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	if err := gocsv.MarshalWithoutHeaders(records, file); err != nil {
+	if err := gocsv.MarshalWithoutHeaders(data, file); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// openFile opens the record file and removes record files that exceed the total size.
-func (s *storage) openFile() (*os.File, error) {
-	fileInfo, err := os.Stat(s.filename)
+// openFile opens the data file and removes data files that exceed the total size.
+func (s *storage) openFile(data any) (*os.File, error) {
+
+	var filename string
+	switch data.(type) {
+	case Record:
+		filename = s.recordFilename
+	case Probes:
+		filename = s.probesFilename
+	}
+	fileInfo, err := os.Stat(filename)
 	if err != nil {
 		return nil, err
 	}
 
 	if s.maxSize <= fileInfo.Size() {
-		if err := os.Rename(s.filename, s.backupFilename()); err != nil {
+		if err := os.Rename(filename, s.backupFilename(data)); err != nil {
 			return nil, err
 		}
 	}
 
-	fileInfos, err := s.backups()
+	fileInfos, err := s.backups(data)
 	if err != nil {
 		return nil, err
 	}
@@ -264,7 +345,7 @@ func (s *storage) openFile() (*os.File, error) {
 		}
 	}
 
-	file, err := os.OpenFile(s.filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		return nil, err
 	}
@@ -273,20 +354,37 @@ func (s *storage) openFile() (*os.File, error) {
 }
 
 // backupFilename generates file name of backup files.
-func (s *storage) backupFilename() string {
+func (s *storage) backupFilename(data any) string {
 	timestamp := time.Now().Format(backupTimeFormat)
-	return filepath.Join(s.baseDir, fmt.Sprintf("%s-%s.%s", RecordFilePrefix, timestamp, RecordFileExt))
+	switch data.(type) {
+	case Record:
+		return filepath.Join(s.baseDir, fmt.Sprintf("%s-%s.%s", RecordFilePrefix, timestamp, RecordFileExt))
+	case Probes:
+		return filepath.Join(s.baseDir, fmt.Sprintf("%s-%s.%s", ProbesFilePrefix, timestamp, ProbesFileExt))
+	default:
+		return ""
+	}
 }
 
 // backupFilename returns backup file information.
-func (s *storage) backups() ([]fs.FileInfo, error) {
+func (s *storage) backups(data any) ([]fs.FileInfo, error) {
 	fileInfos, err := ioutil.ReadDir(s.baseDir)
 	if err != nil {
 		return nil, err
 	}
 
-	var backups []fs.FileInfo
-	regexp := regexp.MustCompile(RecordFilePrefix)
+	var (
+		backups    []fs.FileInfo
+		filePrefix string
+	)
+	switch data.(type) {
+	case Record:
+		filePrefix = RecordFilePrefix
+	case Probes:
+		filePrefix = ProbesFilePrefix
+	}
+
+	regexp := regexp.MustCompile(filePrefix)
 	for _, fileInfo := range fileInfos {
 		if !fileInfo.IsDir() && regexp.MatchString(fileInfo.Name()) {
 			backups = append(backups, fileInfo)
