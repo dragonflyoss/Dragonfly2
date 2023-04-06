6adda51e73885d4d60d5199728b94f8413c5b169
XZ <834756128@qq.com>
2023-04-06 19:46:00 +0800

    refactor: implements the generic interface
    Signed-off-by: XZ <834756128@qq.com>

--- scheduler/storage/storage.go
+++ scheduler/storage/storage.go
@@ -385,7 +385,7 @@ func (s *storage) create(data any) error {
 		}
 		defer file.Close()
 
-		if err := gocsv.MarshalWithoutHeaders(data.(Record), file); err != nil {
+		if err := gocsv.MarshalWithoutHeaders(data, file); err != nil {
 			return err
 		}
 
@@ -397,7 +397,7 @@ func (s *storage) create(data any) error {
 		}
 		defer file.Close()
 
-		if err := gocsv.MarshalWithoutHeaders(data.(Probes), file); err != nil {
+		if err := gocsv.MarshalWithoutHeaders(data, file); err != nil {
 			return err
 		}
 
