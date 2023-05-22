7a9920520a75332efa4899ab33083fbaa62cb00a
MinH-09 <2107139596@qq.com>
2023-05-22 10:36:10 +0800

    feat: announcer adds announcerToTrainer
    Signed-off-by: MinH-09 <2107139596@qq.com>

--- scheduler/announcer/announcer.go
+++ scheduler/announcer/announcer.go
@@ -160,6 +160,7 @@ func (a *announcer) announceToTrainer() {
 						}
 						wg.Done()
 					}()
+
 					go func() {
 						if err := a.transferNetworkTopologyToTrainer(); err != nil {
 							logger.Errorf("transfer networkTopology to trainer failed: %s", err.Error())
