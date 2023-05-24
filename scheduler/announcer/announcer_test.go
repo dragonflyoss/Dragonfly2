9f22ffce7394fe5fdcbaa0eaeab5c7bce91547f1
MinH-09 <2107139596@qq.com>
2023-05-24 17:16:23 +0800

    feat: announcer adds tests
    Signed-off-by: MinH-09 <2107139596@qq.com>
    Signed-off-by: huangmin <2107139596@qq.com>

--- scheduler/announcer/announcer_test.go
+++ scheduler/announcer/announcer_test.go
@@ -22,6 +22,7 @@ import (
 	"io"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -118,6 +119,268 @@ func TestAnnouncer_New(t *testing.T) {
 
 			a, err := New(tc.config, mockManagerClient, mockStorage)
 			tc.expect(t, a, err)
+		})
+	}
+}
+
+func TestAnnouncer_Serve(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+	mockTrainerClient := trainerclientmocks.NewMockV1(ctl)
+
+	tests := []struct {
+		name   string
+		config *config.Config
+		data   []byte
+		option []Option
+		sleep  func()
+		mock   func(stream trainerv1.Trainer_TrainClient, data []byte, m *managerclientmocks.MockV2MockRecorder, mtc *trainerclientmocks.MockV1MockRecorder, ms *storagemocks.MockStorageMockRecorder, mt *trainerv1mocks.MockTrainer_TrainClientMockRecorder)
+		except func(a Announcer)
+	}{
+		{
+			name: "started announcer server success",
+			config: &config.Config{
+				Server: config.ServerConfig{
+					Host:          "localhost",
+					AdvertiseIP:   net.ParseIP("127.0.0.1"),
+					AdvertisePort: 8004,
+					Port:          8080,
+				},
+				Host: config.HostConfig{
+					IDC:      "foo",
+					Location: "bar",
+				},
+				Manager: config.ManagerConfig{
+					KeepAlive: config.KeepAliveConfig{
+						Interval: 500 * time.Millisecond,
+					},
+					SchedulerClusterID: 1,
+				},
+				Trainer: config.TrainerConfig{
+					Interval:      500 * time.Millisecond,
+					UploadTimeout: 10 * time.Second,
+				},
+			},
+			data:   []byte("buffer data"),
+			option: []Option{WithTrainerClient(mockTrainerClient)},
+			sleep: func() {
+				time.Sleep(100 * time.Millisecond)
+			},
+			mock: func(stream trainerv1.Trainer_TrainClient, data []byte, m *managerclientmocks.MockV2MockRecorder, mtc *trainerclientmocks.MockV1MockRecorder, ms *storagemocks.MockStorageMockRecorder, mt *trainerv1mocks.MockTrainer_TrainClientMockRecorder) {
+				m.UpdateScheduler(gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+				m.KeepAlive(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+				mtc.Train(gomock.Any()).Return(stream, nil).AnyTimes()
+				ms.OpenDownload().Return(io.NopCloser(bytes.NewBuffer(data)), nil).AnyTimes()
+				ms.OpenNetworkTopology().Return(io.NopCloser(bytes.NewBuffer(data)), nil).AnyTimes()
+				mt.Send(gomock.Any()).Return(nil).AnyTimes()
+				mt.CloseAndRecv().Return(nil, nil).AnyTimes()
+			},
+			except: func(a Announcer) {
+				go func() {
+					a.Serve()
+				}()
+			},
+		},
+		{
+			name: "started announcer server success without trainer client",
+			config: &config.Config{
+				Server: config.ServerConfig{
+					Host:          "localhost",
+					AdvertiseIP:   net.ParseIP("127.0.0.1"),
+					AdvertisePort: 8004,
+					Port:          8080,
+				},
+				Host: config.HostConfig{
+					IDC:      "foo",
+					Location: "bar",
+				},
+				Manager: config.ManagerConfig{
+					KeepAlive: config.KeepAliveConfig{
+						Interval: 500 * time.Millisecond,
+					},
+					SchedulerClusterID: 1,
+				},
+			},
+			data:   []byte("buffer data"),
+			option: []Option{},
+			sleep: func() {
+				time.Sleep(100 * time.Millisecond)
+			},
+			mock: func(stream trainerv1.Trainer_TrainClient, data []byte, m *managerclientmocks.MockV2MockRecorder, mtc *trainerclientmocks.MockV1MockRecorder, ms *storagemocks.MockStorageMockRecorder, mt *trainerv1mocks.MockTrainer_TrainClientMockRecorder) {
+				m.UpdateScheduler(gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+				m.KeepAlive(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+			},
+			except: func(a Announcer) {
+				go func() {
+					a.Serve()
+				}()
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			stream := trainerv1mocks.NewMockTrainer_TrainClient(ctl)
+			mockManagerClient := managerclientmocks.NewMockV2(ctl)
+			mockStorage := storagemocks.NewMockStorage(ctl)
+			tc.mock(stream, tc.data, mockManagerClient.EXPECT(), mockTrainerClient.EXPECT(), mockStorage.EXPECT(), stream.EXPECT())
+			a, err := New(tc.config, mockManagerClient, mockStorage, tc.option...)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			tc.except(a)
+			tc.sleep()
+			a.Stop()
+		})
+	}
+}
+
+func TestAnnouncer_announceToManager(t *testing.T) {
+	tests := []struct {
+		name   string
+		config *config.Config
+		sleep  func()
+		mock   func(m *managerclientmocks.MockV2MockRecorder)
+		expect func(a Announcer, err error)
+	}{
+		{
+			name: "announce to manager success",
+			config: &config.Config{
+				Server: config.ServerConfig{
+					Host:        "localhost",
+					AdvertiseIP: net.ParseIP("127.0.0.1"),
+				},
+				Manager: config.ManagerConfig{
+					KeepAlive: config.KeepAliveConfig{
+						Interval: 500 * time.Millisecond,
+					},
+					SchedulerClusterID: 1,
+				},
+			},
+			sleep: func() {
+				time.Sleep(100 * time.Millisecond)
+			},
+			mock: func(m *managerclientmocks.MockV2MockRecorder) {
+				m.UpdateScheduler(gomock.Any(), gomock.Any()).Return(nil, nil).Times(1)
+				m.KeepAlive(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+			},
+			expect: func(a Announcer, err error) {
+				err = a.(*announcer).announceToManager()
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			mockManagerClient := managerclientmocks.NewMockV2(ctl)
+			mockTrainerClient := trainerclientmocks.NewMockV1(ctl)
+			mockStorage := storagemocks.NewMockStorage(ctl)
+			tc.mock(mockManagerClient.EXPECT())
+
+			a, err := New(tc.config, mockManagerClient, mockStorage, WithTrainerClient(mockTrainerClient))
+			if err != nil {
+				t.Fatal(err)
+			}
+			tc.sleep()
+			tc.expect(a, err)
+			a.Stop()
+		})
+	}
+}
+
+func TestAnnouncer_announceToTrainer(t *testing.T) {
+	tests := []struct {
+		name   string
+		config *config.Config
+		data   []byte
+		sleep  func()
+		mock   func(stream trainerv1.Trainer_TrainClient, data []byte, m *managerclientmocks.MockV2MockRecorder, mtc *trainerclientmocks.MockV1MockRecorder, ms *storagemocks.MockStorageMockRecorder, mt *trainerv1mocks.MockTrainer_TrainClientMockRecorder)
+		except func(announcer Announcer)
+	}{
+		{
+			name: "announce to trainer failed",
+			config: &config.Config{
+				Server: config.ServerConfig{
+					Host:        "localhost",
+					AdvertiseIP: net.ParseIP("127.0.0.1"),
+				},
+				Manager: config.ManagerConfig{
+					SchedulerClusterID: 1,
+				},
+				Trainer: config.TrainerConfig{
+					Interval:      50 * time.Millisecond,
+					UploadTimeout: 10 * time.Second,
+				},
+			},
+			data: []byte("buffer data"),
+			sleep: func() {
+				time.Sleep(100 * time.Millisecond)
+			},
+			mock: func(stream trainerv1.Trainer_TrainClient, data []byte, m *managerclientmocks.MockV2MockRecorder, mtc *trainerclientmocks.MockV1MockRecorder, ms *storagemocks.MockStorageMockRecorder, mt *trainerv1mocks.MockTrainer_TrainClientMockRecorder) {
+				m.UpdateScheduler(gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+				mtc.Train(gomock.Any()).Return(nil, errors.New("foo")).AnyTimes()
+			},
+			except: func(a Announcer) {
+				go func() {
+					a.(*announcer).announceToTrainer()
+				}()
+			},
+		},
+		{
+			name: "announce to trainer success",
+			config: &config.Config{
+				Server: config.ServerConfig{
+					Host:        "localhost",
+					AdvertiseIP: net.ParseIP("127.0.0.1"),
+				},
+				Manager: config.ManagerConfig{
+					SchedulerClusterID: 1,
+				},
+				Trainer: config.TrainerConfig{
+					Interval:      10 * time.Millisecond,
+					UploadTimeout: 1 * time.Second,
+				},
+			},
+			data: []byte("buffer data"),
+			sleep: func() {
+				time.Sleep(100 * time.Millisecond)
+			},
+			mock: func(stream trainerv1.Trainer_TrainClient, data []byte, m *managerclientmocks.MockV2MockRecorder, mtc *trainerclientmocks.MockV1MockRecorder, ms *storagemocks.MockStorageMockRecorder, mt *trainerv1mocks.MockTrainer_TrainClientMockRecorder) {
+				m.UpdateScheduler(gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+				mtc.Train(gomock.Any()).Return(stream, nil).AnyTimes()
+				ms.OpenDownload().Return(io.NopCloser(bytes.NewBuffer(data)), nil).AnyTimes()
+				ms.OpenNetworkTopology().Return(io.NopCloser(bytes.NewBuffer(data)), nil).AnyTimes()
+				mt.Send(gomock.Any()).Return(nil).AnyTimes()
+				mt.CloseAndRecv().Return(nil, nil).AnyTimes()
+			},
+			except: func(a Announcer) {
+				go func() {
+					a.(*announcer).announceToTrainer()
+				}()
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			stream := trainerv1mocks.NewMockTrainer_TrainClient(ctl)
+			mockManagerClient := managerclientmocks.NewMockV2(ctl)
+			mockTrainerClient := trainerclientmocks.NewMockV1(ctl)
+			mockStorage := storagemocks.NewMockStorage(ctl)
+			tc.mock(stream, tc.data, mockManagerClient.EXPECT(), mockTrainerClient.EXPECT(), mockStorage.EXPECT(), stream.EXPECT())
+
+			a, err := New(tc.config, mockManagerClient, mockStorage, WithTrainerClient(mockTrainerClient))
+			if err != nil {
+				t.Fatal(err)
+			}
+			tc.except(a)
+			tc.sleep()
+			a.Stop()
 		})
 	}
 }
@@ -350,7 +613,7 @@ func TestAnnouncer_uploadDownloadToTrainer(t *testing.T) {
 			},
 		},
 		{
-			name: "send download request successed",
+			name: "send download request success",
 			config: &config.Config{
 				Server: config.ServerConfig{
 					Host:        "localhost",
@@ -477,7 +740,7 @@ func TestAnnouncer_uploadNetworkTopologyToTrainer(t *testing.T) {
 			},
 		},
 		{
-			name: "send network topology successed",
+			name: "send network topology success",
 			config: &config.Config{
 				Server: config.ServerConfig{
 					Host:        "localhost",
