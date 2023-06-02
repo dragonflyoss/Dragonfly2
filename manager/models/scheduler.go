7546bfc945af34a64feffc4464794263e321d0eb
Gaius <gaius.qi@gmail.com>
2023-06-02 17:10:36 +0800

    feat: add ip to uk_scheduler index and uk_seed_peer index in manager (#2426)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- manager/models/scheduler.go
+++ manager/models/scheduler.go
@@ -29,7 +29,7 @@ type Scheduler struct {
 	Hostname           string           `gorm:"column:host_name;type:varchar(256);index:uk_scheduler,unique;not null;comment:hostname" json:"host_name"`
 	IDC                string           `gorm:"column:idc;type:varchar(1024);comment:internet data center" json:"idc"`
 	Location           string           `gorm:"column:location;type:varchar(1024);comment:location" json:"location"`
-	IP                 string           `gorm:"column:ip;type:varchar(256);not null;comment:ip address" json:"ip"`
+	IP                 string           `gorm:"column:ip;type:varchar(256);index:uk_scheduler,unique;not null;comment:ip address" json:"ip"`
 	Port               int32            `gorm:"column:port;not null;comment:grpc service listening port" json:"port"`
 	State              string           `gorm:"column:state;type:varchar(256);default:'inactive';comment:service state" json:"state"`
 	Features           Array            `gorm:"column:features;comment:feature flags" json:"features"`
