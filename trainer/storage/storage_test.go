b9340920f601c3adaa6aca47d6b588d5197bdb11
MIchaelFU0403 <1078537664@qq.com>
2023-06-06 14:35:50 +0800

    deal with lint error
    Signed-off-by: MIchaelFU0403 <1078537664@qq.com>

--- trainer/storage/storage_test.go
+++ trainer/storage/storage_test.go
@@ -26,10 +26,11 @@ import (
 	"regexp"
 	"testing"
 
-	schedulerstorage "d7y.io/dragonfly/v2/scheduler/storage"
 	"github.com/gocarina/gocsv"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	schedulerstorage "d7y.io/dragonfly/v2/scheduler/storage"
 )
 
 var (
