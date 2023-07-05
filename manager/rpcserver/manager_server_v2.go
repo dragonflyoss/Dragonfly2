356ac364b13362949ccb83188e598d0e5d05dbdd
MinH-09 <2107139596@qq.com>
2023-07-05 17:49:15 +0800

    feat: manager adds createModel
    Signed-off-by: MinH-09 <2107139596@qq.com>

--- manager/rpcserver/manager_server_v2.go
+++ manager/rpcserver/manager_server_v2.go
@@ -19,7 +19,6 @@ package rpcserver
 import (
 	"bytes"
 	"context"
-	"d7y.io/dragonfly/v2/pkg/digest"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -48,6 +47,7 @@ import (
 	"d7y.io/dragonfly/v2/manager/searcher"
 	"d7y.io/dragonfly/v2/manager/types"
 	pkgcache "d7y.io/dragonfly/v2/pkg/cache"
+	"d7y.io/dragonfly/v2/pkg/digest"
 	"d7y.io/dragonfly/v2/pkg/objectstorage"
 	pkgredis "d7y.io/dragonfly/v2/pkg/redis"
 	"d7y.io/dragonfly/v2/pkg/slices"
