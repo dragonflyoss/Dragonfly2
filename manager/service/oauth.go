b9f72e549249e38f96d439b9d670ff44a20ffd4c
Gaius <gaius.qi@gmail.com>
2023-08-03 14:53:22 +0800

    feat: use unscoped delete for resource in manager
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- manager/service/oauth.go
+++ manager/service/oauth.go
@@ -45,7 +45,7 @@ func (s *service) DestroyOauth(ctx context.Context, id uint) error {
 		return err
 	}
 
-	if err := s.db.WithContext(ctx).Unscoped().Delete(&models.Oauth{}, id).Error; err != nil {
+	if err := s.db.WithContext(ctx).Unscoped().Unscoped().Delete(&models.Oauth{}, id).Error; err != nil {
 		return err
 	}
 
