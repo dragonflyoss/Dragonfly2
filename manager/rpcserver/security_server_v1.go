750b57ffd59778fbead83da5e4cdf9562e24be0a
Gaius <gaius.qi@gmail.com>
2023-08-09 15:32:03 +0800

    fix: manager generates mTLS certificates for arbitrary IP addresses
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- manager/rpcserver/security_server_v1.go
+++ manager/rpcserver/security_server_v1.go
@@ -22,11 +22,9 @@ import (
 	"crypto/x509"
 	"fmt"
 	"math/big"
-	"net"
 	"time"
 
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 
 	securityv1 "d7y.io/api/v2/pkg/apis/security/v1"
@@ -53,24 +51,6 @@ func (s *securityServerV1) IssueCertificate(ctx context.Context, req *securityv1
 		return nil, status.Errorf(codes.Unavailable, "ca is missing for this manager instance")
 	}
 
-	var (
-		ip  string
-		err error
-	)
-	p, ok := peer.FromContext(ctx)
-	if !ok {
-		return nil, status.Errorf(codes.InvalidArgument, "invalid grpc peer info")
-	}
-
-	if addr, ok := p.Addr.(*net.TCPAddr); ok {
-		ip = addr.IP.String()
-	} else {
-		ip, _, err = net.SplitHostPort(p.Addr.String())
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	// Parse csr.
 	csr, err := x509.ParseCertificateRequest(req.Csr)
 	if err != nil {
@@ -78,23 +58,20 @@ func (s *securityServerV1) IssueCertificate(ctx context.Context, req *securityv1
 	}
 
 	// Check csr signature.
-	// TODO check csr common name and so on.
 	if err = csr.CheckSignature(); err != nil {
 		return nil, err
 	}
 	logger.Infof("valid csr: %#v", csr.Subject)
 
+	// Check csr ip address.
+	if len(csr.IPAddresses) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid csr ip address")
+	}
+
+	// Generate serial number.
 	serial, err := rand.Int(rand.Reader, (&big.Int{}).Exp(big.NewInt(2), big.NewInt(159), nil))
 	if err != nil {
 		return nil, err
-	}
-
-	// TODO only valid for peer ip
-	// BTW we need support both of ipv4 and ipv6.
-	ips := csr.IPAddresses
-	if len(ips) == 0 {
-		// Add default connected ip.
-		ips = []net.IP{net.ParseIP(ip)}
 	}
 
 	now := time.Now()
@@ -103,7 +80,7 @@ func (s *securityServerV1) IssueCertificate(ctx context.Context, req *securityv1
 		Subject:               csr.Subject,
 		DNSNames:              csr.DNSNames,
 		EmailAddresses:        csr.EmailAddresses,
-		IPAddresses:           ips,
+		IPAddresses:           csr.IPAddresses,
 		URIs:                  csr.URIs,
 		NotBefore:             now.Add(-10 * time.Minute).UTC(),
 		NotAfter:              now.Add(req.ValidityPeriod.AsDuration()).UTC(),
