ffd32a5bdf75b2fb99e6a5c5a324b78991235c3e
Guangwen Feng <fenggw-fnst@fujitsu.com>
2023-08-16 16:46:21 +0800

    test: add unit tests for dfstore (#2643)
    Signed-off-by: Guangwen Feng <fenggw-fnst@fujitsu.com>

--- client/config/dfstore_test.go
+++ client/config/dfstore_test.go
@@ -15,3 +15,81 @@
  */
 
 package config
+
+import (
+	"testing"
+
+	testifyassert "github.com/stretchr/testify/assert"
+)
+
+func Test_NewDfstore(t *testing.T) {
+	tests := []struct {
+		name   string
+		expect func(t *testing.T, cfg *DfstoreConfig)
+	}{
+		{
+			name: "new dfstore configuration",
+			expect: func(t *testing.T, cfg *DfstoreConfig) {
+				assert := testifyassert.New(t)
+				assert.Equal("http://127.0.0.1:65004", cfg.Endpoint)
+				assert.Equal(3, cfg.MaxReplicas)
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := NewDfstore()
+			tc.expect(t, cfg)
+		})
+	}
+}
+
+func Test_Validate(t *testing.T) {
+	tests := []struct {
+		name   string
+		cfg    *DfstoreConfig
+		expect func(t *testing.T, err error)
+	}{
+		{
+			name: "normal dfsrore",
+			cfg: &DfstoreConfig{
+				Endpoint:    "http://127.0.0.1:65004",
+				MaxReplicas: 3,
+			},
+			expect: func(t *testing.T, err error) {
+				assert := testifyassert.New(t)
+				assert.Nil(err)
+			},
+		},
+		{
+			name: "dfstore without endpoint",
+			cfg: &DfstoreConfig{
+				Endpoint:    "",
+				MaxReplicas: 3,
+			},
+			expect: func(t *testing.T, err error) {
+				assert := testifyassert.New(t)
+				assert.EqualError(err, "dfstore requires parameter endpoint")
+			},
+		},
+		{
+			name: "dfstore with invalid endpoint",
+			cfg: &DfstoreConfig{
+				Endpoint:    "127.0.0.1:65004",
+				MaxReplicas: 3,
+			},
+			expect: func(t *testing.T, err error) {
+				assert := testifyassert.New(t)
+				assert.EqualError(err, "invalid endpoint: parse \"127.0.0.1:65004\": invalid URI for request")
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.cfg.Validate()
+			tc.expect(t, err)
+		})
+	}
+}
