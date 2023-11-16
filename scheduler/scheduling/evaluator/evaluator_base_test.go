30bbf9ce8eb725957f1566b5b5599c12ba8a5c5f
XZ <834756128@qq.com>
2023-11-16 12:55:49 +0800

    fix
    Signed-off-by: XZ <834756128@qq.com>

--- scheduler/scheduling/evaluator/evaluator_base_test.go
+++ scheduler/scheduling/evaluator/evaluator_base_test.go
@@ -204,7 +204,7 @@ func TestEvaluatorBase_EvaluateParents(t *testing.T) {
 			},
 		},
 		{
-			name: "evaluate parents",
+			name: "evaluate single parent",
 			parents: []*resource.Peer{
 				resource.NewPeer(idgen.PeerIDV1("127.0.0.1"), mockResourceConfig,
 					resource.NewTask(mockTaskID, mockTaskURL, mockTaskTag, mockTaskApplication, commonv2.TaskType_DFDAEMON, mockTaskFilters, mockTaskHeader, mockTaskBackToSourceLimit, resource.WithDigest(mockTaskDigest), resource.WithPieceLength(mockTaskPieceLength)),
@@ -229,32 +229,7 @@ func TestEvaluatorBase_EvaluateParents(t *testing.T) {
 			},
 		},
 		{
-			name: "evaluate parents with pieces",
-			parents: []*resource.Peer{
-				resource.NewPeer(idgen.PeerIDV1("127.0.0.1"), mockResourceConfig,
-					resource.NewTask(mockTaskID, mockTaskURL, mockTaskTag, mockTaskApplication, commonv2.TaskType_DFDAEMON, mockTaskFilters, mockTaskHeader, mockTaskBackToSourceLimit, resource.WithDigest(mockTaskDigest), resource.WithPieceLength(mockTaskPieceLength)),
-					resource.NewHost(
-						mockRawSeedHost.ID, mockRawSeedHost.IP, mockRawSeedHost.Hostname,
-						mockRawSeedHost.Port, mockRawSeedHost.DownloadPort, mockRawSeedHost.Type)),
-			},
-			child: resource.NewPeer(idgen.PeerIDV1("127.0.0.1"), mockResourceConfig,
-				resource.NewTask(mockTaskID, mockTaskURL, mockTaskTag, mockTaskApplication, commonv2.TaskType_DFDAEMON, mockTaskFilters, mockTaskHeader, mockTaskBackToSourceLimit, resource.WithDigest(mockTaskDigest), resource.WithPieceLength(mockTaskPieceLength)),
-				resource.NewHost(
-					mockRawHost.ID, mockRawHost.IP, mockRawHost.Hostname,
-					mockRawHost.Port, mockRawHost.DownloadPort, mockRawHost.Type)),
-			totalPieceCount: 1,
-			mock: func(parents []*resource.Peer, child *resource.Peer) {
-				parents[0].FinishedPieces.Set(0)
-			},
-			expect: func(t *testing.T, parents []*resource.Peer) {
-				assert := assert.New(t)
-				assert.Equal(len(parents), 1)
-				assert.Equal(parents[0].Task.ID, mockTaskID)
-				assert.Equal(parents[0].Host.ID, mockRawSeedHost.ID)
-			},
-		},
-		{
-			name: "evaluate five parents",
+			name: "evaluate parents with free upload count",
 			parents: []*resource.Peer{
 				resource.NewPeer(idgen.PeerIDV1("127.0.0.1"), mockResourceConfig,
 					resource.NewTask(mockTaskID, mockTaskURL, mockTaskTag, mockTaskApplication, commonv2.TaskType_DFDAEMON, mockTaskFilters, mockTaskHeader, mockTaskBackToSourceLimit, resource.WithDigest(mockTaskDigest), resource.WithPieceLength(mockTaskPieceLength)),
@@ -289,19 +264,23 @@ func TestEvaluatorBase_EvaluateParents(t *testing.T) {
 					mockRawHost.Port, mockRawHost.DownloadPort, mockRawHost.Type)),
 			totalPieceCount: 1,
 			mock: func(parents []*resource.Peer, child *resource.Peer) {
+				parents[1].Host.ConcurrentUploadCount.Add(4)
+				parents[2].Host.ConcurrentUploadCount.Add(3)
+				parents[3].Host.ConcurrentUploadCount.Add(2)
+				parents[4].Host.ConcurrentUploadCount.Add(1)
 			},
 			expect: func(t *testing.T, parents []*resource.Peer) {
 				assert := assert.New(t)
 				assert.Equal(len(parents), 5)
 				assert.Equal(parents[0].Host.ID, mockRawSeedHost.ID)
-				assert.Equal(parents[1].Host.ID, "bar")
-				assert.Equal(parents[2].Host.ID, "baz")
-				assert.Equal(parents[3].Host.ID, "bac")
-				assert.Equal(parents[4].Host.ID, "bae")
+				assert.Equal(parents[1].Host.ID, "bae")
+				assert.Equal(parents[2].Host.ID, "bac")
+				assert.Equal(parents[3].Host.ID, "baz")
+				assert.Equal(parents[4].Host.ID, "bar")
 			},
 		},
 		{
-			name: "evaluate five parents with pieces",
+			name: "evaluate parents with pieces",
 			parents: []*resource.Peer{
 				resource.NewPeer(idgen.PeerIDV1("127.0.0.1"), mockResourceConfig,
 					resource.NewTask(mockTaskID, mockTaskURL, mockTaskTag, mockTaskApplication, commonv2.TaskType_DFDAEMON, mockTaskFilters, mockTaskHeader, mockTaskBackToSourceLimit, resource.WithDigest(mockTaskDigest), resource.WithPieceLength(mockTaskPieceLength)),
