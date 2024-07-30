4a399fb134e9463383b20eb75dda46408c601bcd
Asklv <boironic@gmail.com>
2024-07-30 13:29:44 +0800

    feat: add delete task job and list tasks job in scheduler job implementation.
    Signed-off-by: Asklv <boironic@gmail.com>

--- internal/job/types.go
+++ internal/job/types.go
@@ -16,11 +16,13 @@
 
 package job
 
+import "d7y.io/dragonfly/v2/scheduler/resource"
+
 type PreheatRequest struct {
 	URL                 string            `json:"url" validate:"required,url"`
 	Tag                 string            `json:"tag" validate:"omitempty"`
 	Digest              string            `json:"digest" validate:"omitempty"`
-	FilteredQueryParams string            `json:"filteredQueryParams" validate:"omitempty"`
+	FilteredQueryParams string            `json:"filtered_query_params" validate:"omitempty"`
 	Headers             map[string]string `json:"headers" validate:"omitempty"`
 	Application         string            `json:"application" validate:"omitempty"`
 	Priority            int32             `json:"priority" validate:"omitempty"`
@@ -28,4 +30,38 @@ type PreheatRequest struct {
 }
 
 type PreheatResponse struct {
+	TaskID string `json:"taskID"`
+}
+
+// ListTasksRequest defines the request parameters for listing tasks.
+type ListTasksRequest struct {
+	TaskID  string `json:"task_id" validate:"required"`
+	Page    int    `json:"page" validate:"required"`
+	PerPage int    `json:"per_page" validate:"required"`
+}
+
+// ListTasksResponse defines the response parameters for listing tasks.
+type ListTasksResponse struct {
+	Peers []*resource.Peer `json:"peers"`
+	Page  int              `json:"page"`
+	Total int              `json:"total"`
+}
+
+// DeleteTaskRequest defines the request parameters for deleting task.
+type DeleteTaskRequest struct {
+	TaskID string `json:"task_id" validate:"required"`
+}
+
+// TaskInfo includes information about a task along with peer details and a description.
+type TaskInfo struct {
+	Task *resource.Task `json:"task"`
+	Peer *resource.Peer `json:"peer"`
+	Desc string         `json:"desc"`
+}
+
+// DeleteTaskResponse represents the response after attempting to delete tasks,
+// categorizing them into successfully and unsuccessfully deleted.
+type DeleteTaskResponse struct {
+	SuccessTasks []*TaskInfo `json:"success_tasks"`
+	FailureTasks []*TaskInfo `json:"failure_tasks"`
 }
