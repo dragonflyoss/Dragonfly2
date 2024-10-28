636135c4656f226d7b49f45cc979f7fa19aebc39
Gaius <gaius.qi@gmail.com>
2024-10-28 21:34:04 +0800

    chore: update golang version to v1.23.0
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hack/build.sh
+++ hack/build.sh
@@ -11,7 +11,7 @@ SCHEDULER_BINARY_NAME=scheduler
 MANAGER_BINARY_NAME=manager
 
 PKG=d7y.io/dragonfly/v2
-BUILD_IMAGE=golang:1.21.1-alpine3.17
+BUILD_IMAGE=golang:1.23.0-alpine3.20
 
 VERSION=$(git rev-parse --short HEAD)
 BUILD_TIME=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
